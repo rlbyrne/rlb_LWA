644f0a013cbc7529d71f29dea3fe841101fdc1a5
Ruby Byrne <rlbyrne@uw.edu>
2021-07-19 13:12:46 -0700

    update file converter

--- dada_to_ms_converter.py
+++ dada_to_ms_converter.py
@@ -12,12 +12,12 @@ datafiles = os.listdir(f'{datafile_path}/{subband_dirs[0]}')
 datafiles.sort()
 outfile_path = '/lustre/rbyrne/2018-03-20_100hr_run_ms_files'
 outfile_ind = 0
-combine_n_files = 10
+combine_n_files = 1
 for fileind, filename in enumerate(datafiles):
 	for subband_ind, subband in enumerate(subband_dirs):
 		if subband_ind==0 and fileind%combine_n_files==0:
 			outfile_ind += 1
-			outfile_name = f'{outfile_path}/2018-03-20_100hr_run_{str(outfile_ind).zfill(4)}.ms'
+			outfile_name = f'{outfile_path}/2018-03-20_100hr_run_{filename[20:36]}.ms'
 			print(f'Saving data to {outfile_name}')
 			os.system(f'dada2ms-tst3 {datafile_path}/{subband}/{filename} {outfile_name} > /dev/null 2>&1')
 		else:
