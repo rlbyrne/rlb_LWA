5b7a87b8e5c6a0c5945899bbdceac18344b90d54
Ruby Byrne <rlbyrne@uw.edu>
2021-11-09 12:45:53 -0800

    add time axis support to dwcal

--- dwcal/delay_weighted_cal.py
+++ dwcal/delay_weighted_cal.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import sys
 import scipy
 import scipy.optimize
+import time
 
 sys.path.append("/Users/ruby/Astro/pyuvdata")
 import pyuvdata
-import time
 
 
 def get_test_data(use_autos=True):
@@ -48,9 +48,10 @@ def get_test_data(use_autos=True):
     model.read_fhd(model_filelist, use_model=model_use_model)
 
     # For testing, use one time and a few frequencies only
-    use_time = model.time_array[200000]
-    use_frequencies = model.freq_array[0, 100:201]
-    model.select(times=use_time, frequencies=use_frequencies)
+    all_times = np.unique(model.time_array)
+    use_times = all_times[int(model.Ntimes/2):int(model.Ntimes/2+3)]
+    use_frequencies = model.freq_array[0, 100:110]
+    model.select(times=use_times, frequencies=use_frequencies)
 
     if not use_autos:  # Remove autocorrelations
         bl_lengths = np.sqrt(np.sum(model.uvw_array ** 2.0, axis=1))
@@ -62,7 +63,7 @@ def get_test_data(use_autos=True):
         print("Reading data...")
         data.read_fhd(data_filelist, use_model=data_use_model)
         print("Done.")
-        data.select(times=use_time, frequencies=use_frequencies)
+        data.select(times=use_times, frequencies=use_frequencies)
         if not use_autos:  # Remove autocorrelations
             bl_lengths = np.sqrt(np.sum(data.uvw_array ** 2.0, axis=1))
             non_autos = np.where(bl_lengths > 0.01)[0]
@@ -71,7 +72,13 @@ def get_test_data(use_autos=True):
         print("Using model for data")
         data = model.copy()
 
-    # Need to check that the baseline ordering agrees between model and data
+    # Ensure ordering matches between the data and model
+    if np.max(np.abs(data.baseline_array - model.baseline_array)) > 0.0:
+        data.reorder_blts()
+        model.reorder_blts()
+    if np.max(np.abs(data.freq_array - model.freq_array)) > 0.0:
+        data.reorder_freqs(channel_order="freq")
+        model.reorder_freqs(channel_order="freq")
 
     return data, model
 
@@ -85,7 +92,7 @@ def initialize_cal(data):
     cal.Njones = 1
     cal.Nspws = 1
     cal.Ntimes = 1
-    cal.ant_array = np.intersect1d(data.ant_1_array, data.ant_2_array)
+    cal.ant_array = np.unique(np.concatenate((data.ant_1_array, data.ant_2_array)))
     cal.antenna_names = data.antenna_names
     cal.antenna_numbers = data.antenna_numbers
     cal.cal_style = "sky"
@@ -133,7 +140,7 @@ def cost_function_dw_cal(
     x,
     Nants,
     Nfreqs,
-    Nblts,
+    Nbls,
     model_visibilities,
     gains_exp_mat_1,
     gains_exp_mat_2,
@@ -149,9 +156,9 @@ def cost_function_dw_cal(
     gains_expanded = np.matmul(gains_exp_mat_1, gains) * np.matmul(
         gains_exp_mat_2, np.conj(gains)
     )
-    res_vec = data_visibilities - gains_expanded * model_visibilities
-    weighted_part2 = np.squeeze(np.matmul(res_vec[:, np.newaxis, :], cov_mat))
-    cost = np.real(np.sum(np.conj(res_vec) * weighted_part2))
+    res_vec = data_visibilities - gains_expanded[np.newaxis, :, :] * model_visibilities
+    weighted_part2 = np.squeeze(np.matmul(res_vec[:, :, np.newaxis, :], cov_mat))
+    cost = np.real(np.sum(np.conj(np.squeeze(res_vec)) * weighted_part2))
 
     print("Cost func. eval.")
 
@@ -162,7 +169,7 @@ def jac_dw_cal(
     x,
     Nants,
     Nfreqs,
-    Nblts,
+    Nbls,
     model_visibilities,
     gains_exp_mat_1,
     gains_exp_mat_2,
@@ -177,15 +184,17 @@ def jac_dw_cal(
 
     gains1_expanded = np.matmul(gains_exp_mat_1, gains)
     gains2_expanded = np.matmul(gains_exp_mat_2, gains)
-    term1_part1 = gains1_expanded * model_visibilities
-    term2_part1 = gains2_expanded * np.conj(model_visibilities)
+    term1_part1 = gains1_expanded[np.newaxis, :, :] * model_visibilities
+    term2_part1 = gains2_expanded[np.newaxis, :, :] * np.conj(model_visibilities)
     cost_term = (
         data_visibilities
         - gains1_expanded * np.conj(gains2_expanded) * model_visibilities
     )
-    weighted_part2 = np.squeeze(np.matmul(cost_term[:, np.newaxis, :], cov_mat))
-    term1 = np.matmul(gains_exp_mat_2.T, term1_part1 * np.conj(weighted_part2))
-    term2 = np.matmul(gains_exp_mat_1.T, term2_part1 * weighted_part2)
+    weighted_part2 = np.squeeze(np.matmul(cost_term[:, :, np.newaxis, :], cov_mat))
+    term1 = np.sum(
+        np.matmul(gains_exp_mat_2.T, term1_part1 * np.conj(weighted_part2)), axis=0
+    )
+    term2 = np.sum(np.matmul(gains_exp_mat_1.T, term2_part1 * weighted_part2), axis=0)
     grad = -2 * (term1 + term2)
 
     grad = np.stack((np.real(grad), np.imag(grad)), axis=0).flatten()
@@ -193,9 +202,7 @@ def jac_dw_cal(
     return grad
 
 
-def reformat_baselines_to_antenna_matrix(
-    bl_array, gains_exp_mat_1, gains_exp_mat_2
-):
+def reformat_baselines_to_antenna_matrix(bl_array, gains_exp_mat_1, gains_exp_mat_2):
     # Reformat an array indexed in baselines into a matrix with antenna indices
 
     (Nbls, Nants) = np.shape(gains_exp_mat_1)
@@ -219,7 +226,7 @@ def hess_dw_cal(
     x,
     Nants,
     Nfreqs,
-    Nblts,
+    Nbls,
     model_visibilities,
     gains_exp_mat_1,
     gains_exp_mat_2,
@@ -235,23 +242,27 @@ def hess_dw_cal(
     gains1_expanded = np.matmul(gains_exp_mat_1, gains)
     gains2_expanded = np.matmul(gains_exp_mat_2, gains)
 
-    gains1_times_model = gains1_expanded * model_visibilities
-    gains2_times_conj_model = gains2_expanded * np.conj(model_visibilities)
+    gains1_times_model = gains1_expanded[np.newaxis, :, :] * model_visibilities
+    gains2_times_conj_model = gains2_expanded[np.newaxis, :, :] * np.conj(
+        model_visibilities
+    )
 
-    term1 = (
-        gains1_times_model[:, np.newaxis, :]
-        * gains2_times_conj_model[:, :, np.newaxis]
-        * np.conj(cov_mat)
+    term1 = np.sum(
+        gains1_times_model[:, :, np.newaxis, :]
+        * gains2_times_conj_model[:, :, :, np.newaxis]
+        * np.conj(cov_mat[np.newaxis, :, :, :]),
+        axis=0
     )
     term1 = reformat_baselines_to_antenna_matrix(
         term1, gains_exp_mat_1, gains_exp_mat_2
     )
     term1 = np.transpose(term1, (1, 0, 2, 3))
 
-    term2 = (
-        gains2_times_conj_model[:, np.newaxis, :]
-        * gains1_times_model[:, :, np.newaxis]
-        * cov_mat
+    term2 = np.sum(
+        gains2_times_conj_model[:, :, np.newaxis, :]
+        * gains1_times_model[:, :, :, np.newaxis]
+        * cov_mat[np.newaxis, :, :, :],
+        axis=0
     )
     term2 = reformat_baselines_to_antenna_matrix(
         term2, gains_exp_mat_1, gains_exp_mat_2
@@ -265,13 +276,19 @@ def hess_dw_cal(
     hess[:, :, :, :, 1] = np.imag(terms1and2)
     hess[:, :, :, :, 2] = -np.real(terms1and2)
 
-    term3 = np.conj(model_visibilities) * np.sum(
-        cov_mat
-        * (
-            data_visibilities
-            - gains1_expanded * np.conj(gains2_expanded) * model_visibilities
-        )[:, :, np.newaxis],
-        axis=2,
+    term3 = np.sum(
+        np.conj(model_visibilities)
+        * np.sum(
+            cov_mat[np.newaxis, :, :, :]
+            * (
+                data_visibilities
+                - gains1_expanded[np.newaxis, :, :]
+                * np.conj(gains2_expanded[np.newaxis, :, :])
+                * model_visibilities
+            )[:, :, :, np.newaxis],
+            axis=3,
+        ),
+        axis=0,
     )
     term3 = reformat_baselines_to_antenna_matrix(
         term3, gains_exp_mat_1, gains_exp_mat_2
@@ -306,7 +323,7 @@ def cost_function_sky_cal(
     x,
     Nants,
     Nfreqs,
-    Nblts,
+    Nbls,
     model_visibilities,
     gains_exp_mat_1,
     gains_exp_mat_2,
@@ -322,7 +339,7 @@ def cost_function_sky_cal(
     gains_expanded = np.matmul(gains_exp_mat_1, gains) * np.matmul(
         gains_exp_mat_2, np.conj(gains)
     )
-    res_vec = data_visibilities - gains_expanded * model_visibilities
+    res_vec = data_visibilities - gains_expanded[np.newaxis, :, :] * model_visibilities
 
     cost = np.sum(np.abs(res_vec) ** 2)
 
@@ -331,28 +348,49 @@ def cost_function_sky_cal(
 
 def calibrate():
 
-    data, model = get_test_data()
+    data, model = get_test_data(use_autos=False)
 
     cal = initialize_cal(data)
 
+    # Format visibilities
+    data_visibilities = np.zeros((data.Ntimes, data.Nbls, cal.Nfreqs), dtype=complex)
+    model_visibilities = np.zeros((data.Ntimes, data.Nbls, cal.Nfreqs), dtype=complex)
+    for time_ind, time_val in enumerate(np.unique(data.time_array)):
+        data_copy = data.copy()
+        model_copy = model.copy()
+        data_copy.select(times=time_val)
+        model_copy.select(times=time_val)
+        data_copy.reorder_blts()
+        model_copy.reorder_blts()
+        data_copy.reorder_freqs(channel_order="freq")
+        model_copy.reorder_freqs(channel_order="freq")
+        if time_ind == 0:
+            metadata_reference = data_copy.copy(metadata_only=True)
+        model_visibilities[time_ind, :, :] = np.squeeze(
+            model_copy.data_array, axis=(1, 3)
+        )
+        data_visibilities[time_ind, :, :] = np.squeeze(
+            data_copy.data_array, axis=(1, 3)
+        )
+
     # Create gains expand matrices
-    gains_exp_mat_1 = np.zeros((data.Nblts, cal.Nants_data), dtype=int)
-    gains_exp_mat_2 = np.zeros((data.Nblts, cal.Nants_data), dtype=int)
-    antenna_list = np.unique([data.ant_1_array, data.ant_2_array])
-    for baseline in range(data.Nblts):
+    gains_exp_mat_1 = np.zeros((metadata_reference.Nbls, cal.Nants_data), dtype=int)
+    gains_exp_mat_2 = np.zeros((metadata_reference.Nbls, cal.Nants_data), dtype=int)
+    antenna_list = np.unique([metadata_reference.ant_1_array, metadata_reference.ant_2_array])
+    for baseline in range(metadata_reference.Nbls):
         gains_exp_mat_1[
-            baseline, np.where(antenna_list == data.ant_1_array[baseline])
+            baseline, np.where(antenna_list == metadata_reference.ant_1_array[baseline])
         ] = 1
         gains_exp_mat_2[
-            baseline, np.where(antenna_list == data.ant_2_array[baseline])
+            baseline, np.where(antenna_list == metadata_reference.ant_2_array[baseline])
         ] = 1
 
     # method = 'CG'
     method = "Newton-CG"
-    # maxiter = 100000
+    xtol = 1e-10  # Defaults to 1e-05
 
     # Initialize gains
-    gain_init_noise = 0.001
+    gain_init_noise = 0.01
     gains_init = np.random.normal(
         1.0, gain_init_noise, size=(cal.Nants_data, cal.Nfreqs)
     ) + 1.0j * np.random.normal(0.0, gain_init_noise, size=(cal.Nants_data, cal.Nfreqs))
@@ -361,8 +399,8 @@ def calibrate():
 
     # Define covariance matrix
     cov_mat = np.identity(cal.Nfreqs)
-    cov_mat = np.repeat(cov_mat[np.newaxis, :, :], data.Nblts, axis=0)
-    cov_mat = cov_mat.reshape((data.Nblts, cal.Nfreqs, cal.Nfreqs))
+    cov_mat = np.repeat(cov_mat[np.newaxis, :, :], metadata_reference.Nbls, axis=0)
+    cov_mat = cov_mat.reshape((metadata_reference.Nbls, cal.Nfreqs, cal.Nfreqs))
 
     # Minimize the cost function
     print("Beginning optimization")
@@ -372,17 +410,17 @@ def calibrate():
         args=(
             cal.Nants_data,
             cal.Nfreqs,
-            data.Nblts,
-            np.squeeze(model.data_array, axis=(1, 3)),
+            metadata_reference.Nbls,
+            model_visibilities,
             gains_exp_mat_1,
             gains_exp_mat_2,
             cov_mat,
-            np.squeeze(data.data_array, axis=(1, 3)),
+            data_visibilities,
         ),
         method=method,
         jac=jac_dw_cal,
         hess=hess_dw_cal,
-        options={"disp": True},
+        options={"disp": True, "xtol": xtol},
     )
     print(result.message)
 
@@ -408,12 +446,8 @@ def calibrate():
 
 def get_calibration_reference():
 
-    cal_file_path = (
-        "/Users/ruby/Astro/FHD_outputs/fhd_rlb_perfreq_cal_Feb2021/calibration/1061316296_cal.sav"
-    )
-    obs_file_path = (
-        "/Users/ruby/Astro/FHD_outputs/fhd_rlb_perfreq_cal_Feb2021/metadata/1061316296_obs.sav"
-    )
+    cal_file_path = "/Users/ruby/Astro/FHD_outputs/fhd_rlb_perfreq_cal_Feb2021/calibration/1061316296_cal.sav"
+    obs_file_path = "/Users/ruby/Astro/FHD_outputs/fhd_rlb_perfreq_cal_Feb2021/metadata/1061316296_obs.sav"
     cal_obj = pyuvdata.UVCal()
     cal_obj.read_fhd_cal(cal_file_path, obs_file_path)
     gains = np.copy(cal_obj.gain_array)
