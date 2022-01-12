2542084c2f73c332dc0dc2824f730a04e3abae55
Ruby Byrne <rlbyrne@uw.edu>
2022-01-12 11:39:07 -0800

    new calibration test

--- dwcal/run_dwcal_wario.py
+++ dwcal/run_dwcal_wario.py
@@ -108,6 +108,37 @@ def run_calibration_Jan11():
     )
 
 
+def run_calibration_Jan12():
+
+    dwcal.calibrate(
+        model_path="/safepool/rbyrne/fhd_outputs/fhd_rlb_model_GLEAM_bright_sources_Dec2021",
+        model_use_model=True,
+        data_path="/safepool/rbyrne/fhd_outputs/fhd_rlb_model_GLEAM_bright_sources_Dec2021",
+        data_use_model=False,
+        obsid="1061316296",
+        pol="XX",
+        use_autos=False,
+        use_wedge_exclusion=False,
+        cal_savefile="/safepool/rbyrne/calibration_outputs/increased_initialization_error/vanilla_cal.calfits",
+        calibrated_data_savefile="/sa/safepool/rbyrne/calibration_outputs/increased_initialization_error/vanilla_cal.uvfits",
+        log_file_path="/safepool/rbyrne/calibration_outputs/increased_initialization_error/vanilla_cal_log.txt",
+    )
+
+    dwcal.calibrate(
+        model_path="/safepool/rbyrne/fhd_outputs/fhd_rlb_model_GLEAM_bright_sources_Dec2021",
+        model_use_model=True,
+        data_path="/safepool/rbyrne/fhd_outputs/fhd_rlb_model_GLEAM_bright_sources_Dec2021",
+        data_use_model=False,
+        obsid="1061316296",
+        pol="XX",
+        use_autos=False,
+        use_wedge_exclusion=True,
+        cal_savefile="/safepool/rbyrne/calibration_outputs/increased_initialization_error/wedge_excluded.calfits",
+        calibrated_data_savefile="/safepool/rbyrne/calibration_outputs/increased_initialization_error/wedge_excluded.uvfits",
+        log_file_path="/safepool/rbyrne/calibration_outputs/increased_initialization_error/wedge_excluded_log.txt",
+    )
+
+
 if __name__ == "__main__":
 
-    run_calibration_Jan11()
+    run_calibration_Jan12()
