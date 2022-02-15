99e09dd7c38b7b527cd85f0135d6313e700cb772
Ruby Byrne <rlbyrne@uw.edu>
2022-02-15 13:22:16 -0800

    chunk files into smaller uvfits

--- convert_raw_LWA_data_to_uvfits.py
+++ convert_raw_LWA_data_to_uvfits.py
@@ -9,6 +9,7 @@ freq = "70MHz"
 start_time_stamp = 191447
 end_time_stamp = 194824
 outfile_name = f"/lustre/rbyrne/LWA_data_02102022/20220210_{freq}_combined.uvfits"
+nfiles_per_uvfits = 12
 
 filenames = os.listdir(data_path)
 use_files = []
@@ -25,14 +26,16 @@ for file_ind, file in enumerate(use_files):
     uv_new.read_ms(f"{data_path}/{file_split[0]}.ms")
     subprocess.call(shlex.split(f"rm -r {data_path}/{file_split[0]}.ms"))
     uv_new.unphase_to_drift()
-    if file_ind == 0:
+    if file_ind % nfiles_per_uvfits == 0:
         uv = uv_new
+	time_stamp = file.split("_")[2]
+	outfile_name = f"/lustre/rbyrne/LWA_data_02102022/20220210_{freq}_{time_stamp}_combined.uvfits"
     else:
         uv = uv + uv_new
-print(uv.check())
-
-uv.instrument = 'OVRO-LWA'
-uv.telescope_name = 'OVRO-LWA'
-uv.set_telescope_params()
-print('Saving file to {}'.format(outfile_name))
-uv.write_uvfits(outfile_name, spoof_nonessential=True)
+    if (file_ind+1) % nfiles_per_uvfits == 0:
+        uv.instrument = 'OVRO-LWA'
+        uv.telescope_name = 'OVRO-LWA'
+        uv.set_telescope_params()
+        print(uv.check())        
+        print('Saving file to {}'.format(outfile_name))
+        uv.write_uvfits(outfile_name, spoof_nonessential=True)
