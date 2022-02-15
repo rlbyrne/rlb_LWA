ae86c740768189484ac78903db7915ba242bbc2c
Ruby Byrne <rlbyrne@uw.edu>
2022-02-14 17:22:37 -0800

    write uvfits

--- convert_raw_LWA_data_to_uvfits.py
+++ convert_raw_LWA_data_to_uvfits.py
@@ -1,11 +1,14 @@
 import pyuvdata
 import os
 import numpy as np
+import subprocess
+import shlex
 
-data_path = "/lustre/rbyrne/LWA_data_02102022"
+data_path = "/lustre/rbyrne/LWA_data_20220210"
 freq = "70MHz"
 start_time_stamp = 191447
 end_time_stamp = 194824
+outfile_name = f"/lustre/rbyrne/LWA_data_02102022/20220210_{freq}_combined.uvfits"
 
 filenames = os.listdir(data_path)
 use_files = []
@@ -15,4 +18,17 @@ for file in filenames:
         if int(file_split[1]) >= start_time_stamp and int(file_split[1]) <= end_time_stamp:
             use_files.append(file)
 
-print(use_files)
+uv = pyuvdata.UVData()
+for file in use_files:
+    subprocess.Popen(shlex.split(f"tar -xvf {data_path}/{file} -C {data_path}"))
+    file_split = file.split(".")
+    uv_new = pyuvdata.UVData()
+	uv_new.read_ms(f"{data_path}/{file_split[0]}.ms")
+    uv = uv + uv_new
+print(uv.check())
+
+uv.instrument = 'OVRO-LWA'
+uv.telescope_name = 'OVRO-LWA'
+uv.set_telescope_params()
+print('Saving file to {}'.format(outfile_name))
+uv.write_uvfits(outfile_name, spoof_nonessential=True)
