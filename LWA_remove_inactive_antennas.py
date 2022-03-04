b0e3958c41d766c2df870b25d13fe822fb9ef2e2
Ruby Byrne <rlbyrne@uw.edu>
2022-03-03 16:55:55 -0800

    avoid combining files

--- LWA_remove_inactive_antennas.py
+++ LWA_remove_inactive_antennas.py
@@ -19,37 +19,22 @@ use_files = [
 Nants = 352
 autocorr_bls = [(ant_ind, ant_ind) for ant_ind in range(Nants)]
 for file_ind, uvfits_file in enumerate(use_files):
-    uv_new = pyuvdata.UVData()
-    uv_new.read_uvfits(
-        f"{data_path}/{uvfits_file}", bls=autocorr_bls
-    )
-    if file_ind == 0:
-        uv = uv_new
-        start_time = np.min(uv.time_array)
-    else:
-        uv_new.phase_to_time(start_time)
-        uv += uv_new
-
-del uv_new
-
-unused_ants = []
-used_ants = []
-for ant_ind in len(Nants):
-    ant_name = uv.antenna_names[uv.ant_1_array[ant_ind]]
-    avg_autocorr = np.mean(np.abs(uv.data_array[ant_ind, 0, :, :]))
-    if avg_autocorr < 1.:
-        unused_ants.append(ant_name)
-    else:
-        used_ants.append(ant_name)
-
-del uv
-
-print("Used antennas: {used_ants}")
-print("Unused antennas: {unused_ants}")
-
-for file_ind, uvfits_file in enumerate(use_files):
     uv = pyuvdata.UVData()
     uv.read_uvfits(
-        f"{data_path}/{uvfits_file}", antenna_names=used_ants
+        f"{data_path}/{uvfits_file}", bls=autocorr_bls
     )
+
+    unused_ants = []
+    used_ants = []
+    for ant_ind in len(Nants):
+        ant_name = uv.antenna_names[uv.ant_1_array[ant_ind]]
+        avg_autocorr = np.mean(np.abs(uv.data_array[ant_ind, 0, :, :]))
+        if avg_autocorr < 1.:
+            unused_ants.append(ant_name)
+        else:
+            used_ants.append(ant_name)
+
+    print("Used antennas: {used_ants}")
+    print("Unused antennas: {unused_ants}")
+
     uv.write_uvfits(f"{save_path}/{uvfits_file}")
