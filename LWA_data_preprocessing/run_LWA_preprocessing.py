fc6068183c0c4565681b170cd6ae1ccaa41263a7
Ruby Byrne <rlbyrne@uw.edu>
2022-04-01 15:56:26 -0700

    increase flagging threshold

--- LWA_data_preprocessing/run_LWA_preprocessing.py
+++ LWA_data_preprocessing/run_LWA_preprocessing.py
@@ -109,6 +109,7 @@ def ssins_flagging_Mar31():
             ]
             uvd = LWA_preprocessing.convert_raw_ms_to_uvdata(ms_group_full_paths)
 
+            # Plot autocorrelations before any preprocessing
             LWA_preprocessing.plot_autocorrelations(
                 uvd,
                 plot_save_path=autos_plot_save_path,
@@ -117,10 +118,12 @@ def ssins_flagging_Mar31():
                 plot_legend=False,
             )
 
+            # Remove inactive antennas
             LWA_preprocessing.remove_inactive_antennas(
                 uvd, autocorr_thresh=10.0, inplace=True
             )
 
+            # Replot autocorrelations with antenna removal
             LWA_preprocessing.plot_autocorrelations(
                 uvd,
                 plot_save_path=autos_plot_save_path,
@@ -129,9 +132,10 @@ def ssins_flagging_Mar31():
                 plot_legend=False,
             )
 
+            # RFI flagging
             LWA_preprocessing.ssins_flagging(
                 uvd,
-                sig_thresh=10.0,  # Flagging threshold in std. dev.
+                sig_thresh=20.0,  # Flagging threshold in std. dev.
                 inplace=True,
                 plot_no_flags=True,
                 plot_orig_flags=True,
@@ -139,12 +143,8 @@ def ssins_flagging_Mar31():
                 plot_save_path=ssins_plot_save_path,
                 plot_file_prefix=plot_prefix,
             )
-            uvd.write_uvfits(
-                f"{output_path}/{file_split}.uvfits",
-                force_phase=True,
-                spoof_nonessential=True,
-            )
 
+            # Replot autocorrelations with flagging
             LWA_preprocessing.plot_autocorrelations(
                 uvd,
                 plot_save_path=autos_plot_save_path,
@@ -153,6 +153,15 @@ def ssins_flagging_Mar31():
                 plot_legend=False,
                 plot_flagged_data=False,
             )
+
+            # Write uvfits
+            uvd.write_uvfits(
+                f"{output_path}/{file_split}.uvfits",
+                force_phase=True,
+                spoof_nonessential=True,
+            )
+
+
 
 
 if __name__ == "__main__":
