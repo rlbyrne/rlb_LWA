ffd56e4bd73c1f6097779434e9b00bc93bc161ea
Ruby Byrne <rlbyrne@uw.edu>
2022-04-19 16:01:20 -0700

    fix m-mode map coordinate system

--- healpix_map_convert_fits_to_skyh5.py
+++ healpix_map_convert_fits_to_skyh5.py
@@ -1,6 +1,5 @@
 from astropy.io import fits
 import numpy as np
-import matplotlib.pyplot as plt
 import healpy as hp
 import pyradiosky
 import astropy.units as units
@@ -13,19 +12,43 @@ def convert_fits_to_pyradiosky(fits_filepath, freq_mhz):
     map_data = np.array(file_contents[1].data, dtype=float)
     ordering = file_contents[1].header["ORDERING"].lower()
     nside = int(file_contents[1].header["NSIDE"])
+    npix = hp.nside2npix(nside)
+    coordsys = file_contents[1].header["COORDSYS"]
     file_contents.close()
+
+    # Map must be in equatorial coordinates
+    # COORDSYS definitions: G = galactic, E = ecliptic, C = celestial = equatorial
+    if coordsys != "C":
+        if coordsys == "G":  # Remap to equatorial coordinates
+            if ordering == "nest":
+                nest = True
+            elif ordering == "ring":
+                nest = False
+            else:
+                print("WARNING: Unknown ordering. Assuming ring ordering.")
+                nest = False
+            # Get pixel coordinates
+            theta_gal, phi_gal = hp.pixelfunc.pix2ang(nside, np.arange(npix), nest=nest)
+            # Convert pixel coordinates
+            rot = hp.rotator.Rotator(coord=["C", "G"])
+            theta_eq, phi_eq = rot(theta_gal, phi_gal)
+            # Interpolate map to new pixel locations
+            map_data = hp.get_interp_val(map_data, theta_eq, phi_eq, nest=nest)
+        else:
+            print("WARNING: Unknown coordsys. No reordering applied.")
 
     skymodel = pyradiosky.SkyModel()
     skymodel.component_type = "healpix"
     skymodel.nside = nside
     skymodel.hpx_order = ordering
     skymodel.Nfreqs = 1
-    skymodel.Ncomponents = hp.nside2npix(nside)
+    skymodel.Ncomponents = npix
     skymodel.freq_array = Quantity(np.full(skymodel.Nfreqs, freq_mhz * 1e6), "hertz")
     skymodel.stokes = Quantity(
         np.zeros((4, skymodel.Nfreqs, skymodel.Ncomponents)), "Kelvin"
     )
     skymodel.stokes[0, 0, :] = map_data * units.Kelvin
+    skymodel.hpx_inds = np.arange(skymodel.Ncomponents)
 
     return skymodel
 
@@ -38,4 +61,5 @@ if __name__ == "__main__":
     skymodel.write_skyh5(
         "/Users/ruby/Astro/mmode_maps_eastwood/ovro_lwa_sky_map_73.152MHz.skyh5",
         run_check=False,
+        clobber=True,
     )
