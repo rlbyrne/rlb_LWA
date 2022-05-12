31b0d0a067bf24da2cfb968c60274de08f1223a1
Ruby Byrne <rlbyrne@uw.edu>
2022-05-11 18:19:49 -0700

    create uvfits from dwcal tests

---
+++ make_dwcal_test_uvfits.py
@@ -0,0 +1,51 @@
+import pyuvdata
+from pyuvdata import utils
+
+data_path = "/safepool/rbyrne/fhd_outputs/fhd_rlb_model_GLEAM_Apr2022"
+data_use_model = True
+obsid = "1061316296"
+pol = "XX"
+
+data = pyuvdata.UVData()
+filelist = [
+    "{}/{}".format(data_path, file)
+    for file in [
+        "vis_data/{}_vis_{}.sav".format(obsid, pol),
+        "vis_data/{}_vis_model_{}.sav".format(obsid, pol),
+        "vis_data/{}_flags.sav".format(obsid),
+        "metadata/{}_params.sav".format(obsid),
+        "metadata/{}_settings.txt".format(obsid),
+        "metadata/{}_layout.sav".format(obsid),
+    ]
+]
+data.read_fhd(data_filelist, use_model=data_use_model)
+
+cal_filenames = [
+    "/safepool/rbyrne/calibration_outputs/caltest_Apr12/vanilla_cal.calfits",
+    "/safepool/rbyrne/calibration_outputs/caltest_Apr12/wedge_excluded.calfits",
+    "/safepool/rbyrne/calibration_outputs/random_gains_test_Apr25/vanilla_cal.calfits",
+    "/safepool/rbyrne/calibration_outputs/random_gains_test_Apr25/wedge_excluded.calfits",
+    "/safepool/rbyrne/calibration_outputs/gain_ripple_test_May6/vanilla_cal.calfits",
+    "/safepool/rbyrne/calibration_outputs/gain_ripple_test_May6/wedge_excluded.calfits",
+]
+
+uvfits_output_filenames = [
+    "unity_gains_diagonal.uvfits",
+    "unity_gains_dwcal.uvfits",
+    "random_gains_diagonal.uvfits",
+    "random_gains_dwcal.uvfits",
+    "ripple_gains_diagonal.uvfits",
+    "ripple_gains_dwcal.uvfits",
+]
+uvfits_output_dir = "/safepool/rbyrne/calibration_outputs/uvfits"
+uvfits_output_filenames = [
+    f"{uvfits_output_dir}/{filename}" for filename in uvfits_output_filenames
+]
+
+for ind in range(len(cal_filenames)):
+    cal = pyuvdata.UVCal()
+    cal.read_calfits(cal_filenames[ind])
+    data_calibrated = pyuvdata.utils.uvcalibrate(
+        data, cal, inplace=False, time_check=False
+    )
+    data_calibrated.write_uvfits(uvfits_output_filenames[ind])
