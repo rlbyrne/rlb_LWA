826ecd470cdef904cc72b51d0b0cdafd91ab80ed
Ruby Byrne <rlbyrne@uw.edu>
2022-11-10 14:04:52 -0800

    new FHD jobs

--- FHD_wrappers/run_FHD_wario.py
+++ FHD_wrappers/run_FHD_wario.py
@@ -6,17 +6,11 @@ obsids_list = [
     "random_gains_uncalib",
     "random_gains_dwcal",
     "random_gains_diagonal",
-    "ripple_gains_uncalib",
-    "ripple_gains_dwcal",
-    "ripple_gains_diagonal",
-    "unity_gains_uncalib",
-    "unity_gains_dwcal",
-    #"unity_gains_diagonal"
 ]
-versions_list = ["rlb_cal_sims_Jun2022"]
+versions_list = ["rlb_cal_sims_2pol_Nov2022"]
 uvfits_path = "/safepool/rbyrne/calibration_outputs/caltest_Jun17"
 outdir = "/safepool/rbyrne/fhd_outputs"
-run_fhd = False
+run_fhd = True
 run_eppsilon = True
 
 # Define wrappers
