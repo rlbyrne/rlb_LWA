585fb9dd4f636eaa1710b5e781864ad23ee8adaa
Ruby Byrne <rlbyrne@uw.edu>
2022-11-10 14:17:21 -0800

    reformat diffuse map with spectral index

--- array_simulations.py
+++ array_simulations.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from pyuvsim.telescope import BeamList
+from astropy.units import Quantity
 
 
 def create_random_array(
@@ -192,6 +193,12 @@ if __name__ == "__main__":
         diffuse_map = pyradiosky.SkyModel()
         print("Reading map")
         diffuse_map.read_skyh5(healpix_map_path)
+        # Reformat the map a bit to be compatible with pyuvsim
+        diffuse_map.spectral_type = "spectral_index"
+        diffuse_map.spectral_index = np.full(diffuse_map.Ncomponents, -0.8)
+        diffuse_map.reference_frequency = Quantity(
+            np.full(diffuse_map.Ncomponents, diffuse_map.freq_array), "Hz"
+        )
         print("Starting diffuse simulation")
         diffuse_sim_uv = pyuvsim.uvsim.run_uvdata_uvsim(
             input_uv=uv,
