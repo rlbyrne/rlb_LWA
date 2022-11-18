49e5610b9b93fa84961116270cbabaaaac09c583
Ruby Byrne <rlbyrne@uw.edu>
2022-11-17 16:31:16 -0800

    supply beam parameter

--- pyuvsim_testing.py
+++ pyuvsim_testing.py
@@ -11,7 +11,7 @@ from astropy.units import Quantity
 uv = pyuvdata.UVData()
 uv.read_uvfits("/safepool/rbyrne/mwa_data/1061316296.uvfits")
 
-airy_beam = pyuvsim.AnalyticBeam("airy", diameter=diameter_m)
+airy_beam = pyuvsim.AnalyticBeam("airy", diameter=14.)
 airy_beam.peak_normalize()
 
 healpix_map_path = "/safepool/rbyrne/diffuse_map.skyh5"
