f25741803e617c7cfb4a5d0ca29fc36e9f9e0cba
Ruby Byrne <rlbyrne@uw.edu>
2023-04-20 15:30:59 -0700

    formatting

--- uv_density_simulations/reduce_uv_extent.py
+++ uv_density_simulations/reduce_uv_extent.py
@@ -3,14 +3,18 @@ import numpy as np
 
 
 uv_spacings = ["0.5"]
-max_bl_wl = 80.
+max_bl_wl = 80.0
 for spacing in uv_spacings:
-    filename = f"/safepool/rbyrne/uv_density_simulations/sim_uv_spacing_{spacing}.uvfits"
+    filename = (
+        f"/safepool/rbyrne/uv_density_simulations/sim_uv_spacing_{spacing}.uvfits"
+    )
     uv = pyuvdata.UVData()
     uv.read_uvfits(filename)
     freq = np.mean(uv.freq_array)
     wavelength = 3e8 / freq
-    bl_lengths_wl = np.sqrt(np.sum(np.abs(uv.uvw_array)**2, axis=1)) / wavelength
+    bl_lengths_wl = np.sqrt(np.sum(np.abs(uv.uvw_array) ** 2, axis=1)) / wavelength
     keep_bl = np.where(bl_lengths_wl <= max_bl_wl)[0]
     uv.select(blt_inds=keep_bl, inplace=True)
-    uv.write_uvfits(f"/safepool/rbyrne/uv_density_simulations/sim_uv_spacing_{spacing}_short_bls.uvfits")
+    uv.write_uvfits(
+        f"/safepool/rbyrne/uv_density_simulations/sim_uv_spacing_{spacing}_short_bls.uvfits"
+    )
