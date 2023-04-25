faa2f8ea5b616b59f8d0fe51a603cbc802234ef6
Ruby Byrne <rlbyrne@uw.edu>
2023-04-25 14:03:08 -0700

    update reduce uv extent

--- uv_density_simulations/reduce_uv_extent.py
+++ uv_density_simulations/reduce_uv_extent.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 uv_spacings = ["0.5"]
-max_bl_wl = 80.0
+max_bl_wl = 50.0
 for spacing in uv_spacings:
     filename = (
         f"/safepool/rbyrne/uv_density_simulations/sim_uv_spacing_{spacing}.uvfits"
@@ -15,6 +15,11 @@ for spacing in uv_spacings:
     bl_lengths_wl = np.sqrt(np.sum(np.abs(uv.uvw_array) ** 2, axis=1)) / wavelength
     keep_bl = np.where(bl_lengths_wl <= max_bl_wl)[0]
     uv.select(blt_inds=keep_bl, inplace=True)
+    print(f"Reducing total number of antennas from {uv.Nants_telescope} to {uv.Nants_data}.)
+    uv.Nants_telescope = uv.Nants_data
+    uv.antenna_numbers = np.arange(uv.Nants_data)
+    uv.antenna_names = np.array([str(ind) for ind in np.arange(uv.Nants_data)])
+    uv.check()
     uv.write_uvfits(
         f"/safepool/rbyrne/uv_density_simulations/sim_uv_spacing_{spacing}_short_bls.uvfits"
     )
