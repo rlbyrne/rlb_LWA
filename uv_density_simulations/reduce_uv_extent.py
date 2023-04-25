26ebbc4e5f0fc566505410f25d5210b6946039ff
Ruby Byrne <rlbyrne@uw.edu>
2023-04-25 14:10:23 -0700

    modify antenna positions attribute

--- uv_density_simulations/reduce_uv_extent.py
+++ uv_density_simulations/reduce_uv_extent.py
@@ -15,10 +15,21 @@ for spacing in uv_spacings:
     bl_lengths_wl = np.sqrt(np.sum(np.abs(uv.uvw_array) ** 2, axis=1)) / wavelength
     keep_bl = np.where(bl_lengths_wl <= max_bl_wl)[0]
     uv.select(blt_inds=keep_bl, inplace=True)
-    print(f"Reducing total number of antennas from {uv.Nants_telescope} to {uv.Nants_data}.")
+    print(
+        f"Reducing total number of antennas from {uv.Nants_telescope} to {uv.Nants_data}."
+    )
+    use_ants = np.unique(np.concatenate((uv.ant_1_array, uv.ant_2_array)))
+    use_ant_inds = np.array(
+        [
+            ind
+            for ind in range(uv.Nants_telescope)
+            if uv.antenna_numbers[ind] in use_ants
+        ]
+    )
     uv.Nants_telescope = uv.Nants_data
-    uv.antenna_numbers = np.arange(uv.Nants_data)
-    uv.antenna_names = np.array([str(ind) for ind in np.arange(uv.Nants_data)])
+    uv.antenna_numbers = uv.antenna_numbers[use_ant_inds]
+    uv.antenna_names = uv.antenna_names[use_ant_inds]
+    uv.antenna_positions = uv.antenna_positions[use_ant_inds, :]
     uv.check()
     uv.write_uvfits(
         f"/safepool/rbyrne/uv_density_simulations/sim_uv_spacing_{spacing}_short_bls.uvfits"
