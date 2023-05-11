980e9879a4c7b73c3f5f68f52157a88d233443f3
Ruby Byrne <rlbyrne@uw.edu>
2023-05-10 22:37:11 -0700

    FHD rerun

--- FHD_wrappers/run_FHD_wario.py
+++ FHD_wrappers/run_FHD_wario.py
@@ -4,9 +4,9 @@ import os
 
 obsids_list = [
     "sim_uv_spacing_10_short_bls",
-    "sim_uv_spacing_5_short_bls",
-    "sim_uv_spacing_1_short_bls",
-    "sim_uv_spacing_0.5_short_bls"
+    #"sim_uv_spacing_5_short_bls",
+    #"sim_uv_spacing_1_short_bls",
+    #"sim_uv_spacing_0.5_short_bls"
 ]
 versions_list = ["rlb_uv_density_sims_beam_error_May2023"]
 uvfits_path = "/safepool/rbyrne/uv_density_simulations"
