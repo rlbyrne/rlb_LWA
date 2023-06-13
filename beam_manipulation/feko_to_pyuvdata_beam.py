00bdb63eae43e997351e4dafde5b02c9d783c3d4
Ruby Byrne <rlbyrne@uw.edu>
2023-06-12 17:25:40 -0700

    support both feeds

--- beam_manipulation/feko_to_pyuvdata_beam.py
+++ beam_manipulation/feko_to_pyuvdata_beam.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pyuvdata
 
-with open("/data05/nmahesh/LWA_x_10to100.ffe", "r") as f:
-    data = f.readlines()
-f.close()
+beam_files = ["/data05/nmahesh/LWA_x_10to100.ffe", "/data05/nmahesh/LWA_y_10to100.ffe"]
 
 freq_array = np.array([], dtype=float)
 theta_array = np.array([], dtype=float)  # Zenith angle
@@ -11,74 +9,90 @@ phi_array = np.array([], dtype=float)  # Azimuth
 etheta_array = np.array([], dtype=complex)
 ephi_array = np.array([], dtype=complex)
 
-start_chunk_lines = np.where(["Configuration Name:" in line for line in data])[0]
+file_start_ind = len(freq_array)
 
-# Debug
-data = data[0:start_chunk_lines[3]]
-start_chunk_lines = start_chunk_lines[0:3]
+for file in beam_files:
 
-for chunk_ind in range(len(start_chunk_lines)):
+    with open(file, "r") as f:
+        data = f.readlines()
+    f.close()
 
-    if chunk_ind < len(start_chunk_lines) - 1:
-        chunk_lines = np.arange(
-            start_chunk_lines[chunk_ind], start_chunk_lines[chunk_ind + 1]
-        )
-    else:
-        chunk_lines = np.arange(start_chunk_lines[chunk_ind], len(data))
+    start_chunk_lines = np.where(["Configuration Name:" in line for line in data])[0]
 
-    freq_line = (
-        [line_num for line_num in chunk_lines if "Frequency:" in data[line_num]]
-    )[0]
-    freq_hz = float((data[freq_line].split())[-1])
+    # Debug
+    data = data[0 : start_chunk_lines[3]]
+    start_chunk_lines = start_chunk_lines[0:3]
 
-    # Parse header
-    header_intro_line = (
-        [
-            line_num
-            for line_num in chunk_lines
-            if "No. of Header Lines:" in data[line_num]
-        ]
-    )[0]
-    header_len = int((data[header_intro_line].split())[-1])
-    header_line = header_intro_line + header_len
-    header = np.array(data[header_line].strip("#").split())
-    theta_col = np.where(header == '"Theta"')[0][0]
-    phi_col = np.where(header == '"Phi"')[0][0]
-    etheta_real_col = np.where(header == '"Re(Etheta)"')[0][0]
-    etheta_imag_col = np.where(header == '"Im(Etheta)"')[0][0]
-    ephi_real_col = np.where(header == '"Re(Ephi)"')[0][0]
-    ephi_imag_col = np.where(header == '"Im(Ephi)"')[0][0]
+    for chunk_ind in range(len(start_chunk_lines)):
 
-    for data_line in data[header_line + 1 : np.max(chunk_lines)]:
-        data_line_split = data_line.split()
-        if len(data_line_split) == len(header):
-            freq_array = np.append(freq_array, freq_hz)
-            theta_array = np.append(theta_array, float(data_line_split[theta_col]))
-            phi_array = np.append(phi_array, float(data_line_split[phi_col]))
-            etheta_array = np.append(
-                etheta_array,
-                float(data_line_split[etheta_real_col]) + 1j * float(data_line_split[etheta_imag_col])
+        if chunk_ind < len(start_chunk_lines) - 1:
+            chunk_lines = np.arange(
+                start_chunk_lines[chunk_ind], start_chunk_lines[chunk_ind + 1]
             )
-            ephi_array = np.append(
-                ephi_array,
-                float(data_line_split[ephi_real_col]) + 1j * float(data_line_split[ephi_imag_col])
-            )
+        else:
+            chunk_lines = np.arange(start_chunk_lines[chunk_ind], len(data))
+
+        freq_line = (
+            [line_num for line_num in chunk_lines if "Frequency:" in data[line_num]]
+        )[0]
+        freq_hz = float((data[freq_line].split())[-1])
+
+        # Parse header
+        header_intro_line = (
+            [
+                line_num
+                for line_num in chunk_lines
+                if "No. of Header Lines:" in data[line_num]
+            ]
+        )[0]
+        header_len = int((data[header_intro_line].split())[-1])
+        header_line = header_intro_line + header_len
+        header = np.array(data[header_line].strip("#").split())
+        theta_col = np.where(header == '"Theta"')[0][0]
+        phi_col = np.where(header == '"Phi"')[0][0]
+        etheta_real_col = np.where(header == '"Re(Etheta)"')[0][0]
+        etheta_imag_col = np.where(header == '"Im(Etheta)"')[0][0]
+        ephi_real_col = np.where(header == '"Re(Ephi)"')[0][0]
+        ephi_imag_col = np.where(header == '"Im(Ephi)"')[0][0]
+
+        for data_line in data[header_line + 1 : np.max(chunk_lines)]:
+            data_line_split = data_line.split()
+            if len(data_line_split) == len(header):
+                freq_array = np.append(freq_array, freq_hz)
+                theta_array = np.append(theta_array, float(data_line_split[theta_col]))
+                phi_array = np.append(phi_array, float(data_line_split[phi_col]))
+                etheta_array = np.append(
+                    etheta_array,
+                    float(data_line_split[etheta_real_col])
+                    + 1j * float(data_line_split[etheta_imag_col]),
+                )
+                ephi_array = np.append(
+                    ephi_array,
+                    float(data_line_split[ephi_real_col])
+                    + 1j * float(data_line_split[ephi_imag_col]),
+                )
 
 # Reshape array
 theta_axis, theta_indices = np.unique(theta_array, return_index=True)
 phi_axis, phi_indices = np.unique(phi_array, return_index=True)
 freq_axis, freq_indices = np.unique(freq_array, return_index=True)
-beam_vals = np.full((2, len(phi_axis), len(theta_axis), len(freq_axis)), np.nan, dtype=complex)
+jones = np.full(
+    (2, 1, len(freq_axis), len(theta_axis), len(phi_axis)), np.nan, dtype=complex
+)
 for phi_ind, phi in enumerate(phi_axis):
     for theta_ind, theta in enumerate(theta_axis):
         use_indices = np.intersect1d(phi_indices[phi_ind], theta_indices[theta_ind])
         for freq_ind, freq in enumerate(freq_axis):
             index = np.intersect1d(use_indices, freq_indices[freq_ind])
             if len(index) > 0:
-                beam_vals[0, phi_ind, theta_ind, freq_ind] = ephi_array[index[0]]
-                beam_vals[1, phi_ind, theta_ind, freq_ind] = etheta_array[index[0]]
-
-
+                if index[0] >= file_start_ind:
+                    feed_ind = 1
+                else:
+                    feed_ind = 0
+                jones[0, feed_ind, freq_ind, theta_ind, phi_ind] = ephi_array[index[0]]
+                jones[1, feed_ind, freq_ind, theta_ind, phi_ind] = etheta_array[
+                    index[0]
+                ]
 
 if False:
 
@@ -93,22 +107,20 @@ if False:
     beam_obj.data_normalization = "physical"
     beam_obj.feed_name = ""
     beam_obj.feed_version = ""
-    beam_obj.freq_array = (
-        np.copy(freq_axis[np.newaxis, :]) * 1e6
-    )  # Convert from MHz to Hz
+    beam_obj.freq_array = freq_axis[np.newaxis, :]
     beam_obj.history = ""
     beam_obj.model_name = ""
     beam_obj.model_version = ""
     beam_obj.pixel_coordinate_system = "az_za"
     beam_obj.spw_array = [0]
     beam_obj.telescope_name = "LWA"
-    beam_obj.Naxes1 = len(az_axis)
-    beam_obj.Naxes2 = len(za_axis)
+    beam_obj.Naxes1 = len(phi_axis)
+    beam_obj.Naxes2 = len(theta_axis)
     beam_obj.Ncomponents_vec = 2
     beam_obj.Nfeeds = 2
     beam_obj.Npols = 2
-    beam_obj.axis1_array = np.radians(az_axis)
-    beam_obj.axis2_array = np.radians(za_axis)
+    beam_obj.axis1_array = np.radians(phi_axis)
+    beam_obj.axis2_array = np.radians(theta_axis)
     beam_obj.basis_vector_array = np.repeat(
         (
             np.repeat(
@@ -123,4 +135,4 @@ if False:
     beam_obj.peak_normalize()
     beam_obj.check()
 
-    #return beam_obj
+    # return beam_obj
