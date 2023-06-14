06ffc4e00e04370f1c71313f20a0b76c63c4eae6
Ruby Byrne <rlbyrne@uw.edu>
2023-06-14 14:59:53 -0700

    save each frequency individually

--- beam_manipulation/feko_to_pyuvdata_beam.py
+++ beam_manipulation/feko_to_pyuvdata_beam.py
@@ -20,8 +20,8 @@ for file in beam_files:
     start_chunk_lines = np.where(["Configuration Name:" in line for line in data])[0]
 
     # Debug
-    #data = data[0 : start_chunk_lines[2]]
-    #start_chunk_lines = start_chunk_lines[0:2]
+    # data = data[0 : start_chunk_lines[2]]
+    # start_chunk_lines = start_chunk_lines[0:2]
 
     for chunk_ind in range(len(start_chunk_lines)):
 
@@ -36,6 +36,8 @@ for file in beam_files:
             [line_num for line_num in chunk_lines if "Frequency:" in data[line_num]]
         )[0]
         freq_hz = float((data[freq_line].split())[-1])
+
+        print(f"Processing frequency {freq_hz/1e6} MHz.")
 
         # Parse header
         header_intro_line = (
@@ -72,68 +74,89 @@ for file in beam_files:
                     + 1j * float(data_line_split[ephi_imag_col]),
                 )
 
-# Reshape array
-theta_axis, theta_indices = np.unique(theta_array, return_index=True)
-phi_axis, phi_indices = np.unique(phi_array, return_index=True)
-freq_axis, freq_indices = np.unique(freq_array, return_index=True)
-jones = np.full(
-    (2, 2, len(freq_axis), len(theta_axis), len(phi_axis)), np.nan, dtype=complex
-)
-for phi_ind, phi in enumerate(phi_axis):
-    for theta_ind, theta in enumerate(theta_axis):
-        use_indices = np.intersect1d(phi_indices[phi_ind], theta_indices[theta_ind])
-        for freq_ind, freq in enumerate(freq_axis):
-            index = np.intersect1d(use_indices, freq_indices[freq_ind])
-            if len(index) > 0:
-                if index[0] >= file_start_ind:
-                    feed_ind = 1
-                else:
-                    feed_ind = 0
-                jones[0, feed_ind, freq_ind, theta_ind, phi_ind] = ephi_array[index[0]]
-                jones[1, feed_ind, freq_ind, theta_ind, phi_ind] = etheta_array[
-                    index[0]
-                ]
-
-# Clear variables
-freq_array = theta_array = phi_array = etheta_array = ephi_array = None
-
-beam_obj = pyuvdata.UVBeam()
-beam_obj.Naxes_vec = 2
-beam_obj.Nfreqs = len(freq_axis)
-beam_obj.Nspws = 1
-beam_obj.antenna_type = "simple"
-beam_obj.bandpass_array = np.full((1, len(freq_axis)), 1.0)
-beam_obj.beam_type = "efield"
-beam_obj.data_array = np.copy(jones[:, np.newaxis, :, :, :, :])
-beam_obj.data_normalization = "physical"
-beam_obj.feed_name = ""
-beam_obj.feed_version = ""
-beam_obj.freq_array = freq_axis[np.newaxis, :]
-beam_obj.history = ""
-beam_obj.model_name = ""
-beam_obj.model_version = ""
-beam_obj.pixel_coordinate_system = "az_za"
-beam_obj.spw_array = [0]
-beam_obj.telescope_name = "LWA"
-beam_obj.Naxes1 = len(phi_axis)
-beam_obj.Naxes2 = len(theta_axis)
-beam_obj.Ncomponents_vec = 2
-beam_obj.Nfeeds = 2
-beam_obj.Npols = 2
-beam_obj.axis1_array = np.radians(phi_axis)
-beam_obj.axis2_array = np.radians(theta_axis)
-beam_obj.basis_vector_array = np.repeat(
-    (
-        np.repeat(
-            (np.identity(2, dtype=float))[:, :, np.newaxis], len(theta_axis), axis=2
+        # Reshape array
+        print("Reformatting data...")
+        theta_axis, theta_indices = np.unique(theta_array, return_index=True)
+        phi_axis, phi_indices = np.unique(phi_array, return_index=True)
+        freq_axis, freq_indices = np.unique(freq_array, return_index=True)
+        jones = np.full(
+            (2, 2, len(freq_axis), len(theta_axis), len(phi_axis)),
+            np.nan,
+            dtype=complex,
         )
-    )[:, :, :, np.newaxis],
-    len(phi_axis),
-    axis=3,
-)
-beam_obj.feed_array = ["E", "N"]
-beam_obj.x_orientation = "east"
-#beam_obj.peak_normalize()  # Throws an "invalid value encountered in divide" error
-beam_obj.check()
+        for phi_ind, phi in enumerate(phi_axis):
+            for theta_ind, theta in enumerate(theta_axis):
+                use_indices = np.intersect1d(
+                    phi_indices[phi_ind], theta_indices[theta_ind]
+                )
+                for freq_ind, freq in enumerate(freq_axis):
+                    index = np.intersect1d(use_indices, freq_indices[freq_ind])
+                    if len(index) > 0:
+                        if index[0] >= file_start_ind:
+                            feed_ind = 1
+                        else:
+                            feed_ind = 0
+                        jones[0, feed_ind, freq_ind, theta_ind, phi_ind] = ephi_array[
+                            index[0]
+                        ]
+                        jones[1, feed_ind, freq_ind, theta_ind, phi_ind] = etheta_array[
+                            index[0]
+                        ]
 
-beam_obj.write_beamfits("/data05/rbyrne/LWA_10to100.beamfits")
+        # Clear variables
+        freq_array = theta_array = phi_array = etheta_array = ephi_array = None
+
+        print("Generating beam object...")
+        beam_obj = pyuvdata.UVBeam()
+        beam_obj.Naxes_vec = 2
+        beam_obj.Nfreqs = len(freq_axis)
+        beam_obj.Nspws = 1
+        beam_obj.antenna_type = "simple"
+        beam_obj.bandpass_array = np.full((1, len(freq_axis)), 1.0)
+        beam_obj.beam_type = "efield"
+        beam_obj.data_array = np.copy(jones[:, np.newaxis, :, :, :, :])
+        beam_obj.data_normalization = "physical"
+        beam_obj.feed_name = ""
+        beam_obj.feed_version = ""
+        beam_obj.freq_array = freq_axis[np.newaxis, :]
+        beam_obj.history = ""
+        beam_obj.model_name = ""
+        beam_obj.model_version = ""
+        beam_obj.pixel_coordinate_system = "az_za"
+        beam_obj.spw_array = [0]
+        beam_obj.telescope_name = "LWA"
+        beam_obj.Naxes1 = len(phi_axis)
+        beam_obj.Naxes2 = len(theta_axis)
+        beam_obj.Ncomponents_vec = 2
+        beam_obj.Nfeeds = 2
+        beam_obj.Npols = 2
+        beam_obj.axis1_array = np.radians(phi_axis)
+        beam_obj.axis2_array = np.radians(theta_axis)
+        beam_obj.basis_vector_array = np.repeat(
+            (
+                np.repeat(
+                    (np.identity(2, dtype=float))[:, :, np.newaxis],
+                    len(theta_axis),
+                    axis=2,
+                )
+            )[:, :, :, np.newaxis],
+            len(phi_axis),
+            axis=3,
+        )
+        beam_obj.feed_array = ["E", "N"]
+        beam_obj.x_orientation = "east"
+        # beam_obj.peak_normalize()  # Throws an "invalid value encountered in divide" error
+        beam_obj.check()
+
+        print("Writing beam object...")
+        beam_obj.write_beamfits(f"/data05/rbyrne/LWA_10to100_{freq_hz/1e6}MHz.beamfits")
+
+        # Clear variables
+        beam_obj = None
+        theta_axis, theta_indices = None
+        phi_axis, phi_indices = None
+        freq_axis, freq_indices = None
+        jones = None
+
+        print("Done.")
+        print("")
