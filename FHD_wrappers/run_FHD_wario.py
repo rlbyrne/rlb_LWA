cc58fb71fb461b389a5779aa1a7ce0d7dfd4ccdb
Ruby Byrne <rlbyrne@uw.edu>
2023-06-22 13:36:14 -0700

    new FHD test

--- FHD_wrappers/run_FHD_wario.py
+++ FHD_wrappers/run_FHD_wario.py
@@ -5,7 +5,7 @@ import os
 obsids_list = [
     "458_peeled_calibrated"
 ]
-versions_list = ["rlb_image_LWA_data_no_calibration_Jun2023"]
+versions_list = ["rlb_image_LWA_data_no_calibration_all_baselines_Jun2023"]
 uvfits_path = "/safepool/rbyrne/lwa_data"
 outdir = "/safepool/rbyrne/fhd_outputs"
 run_fhd = True
