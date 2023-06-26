04eb2fa3732536961f99259e953d3edf8c8aabae
Ruby Byrne <rlbyrne@uw.edu>
2023-06-26 14:21:51 -0700

    new beam testing runs

--- FHD_wrappers/run_FHD_wario.py
+++ FHD_wrappers/run_FHD_wario.py
@@ -5,7 +5,10 @@ import os
 obsids_list = [
     "458_peeled_calibrated"
 ]
-versions_list = ["rlb_image_LWA_data_natural_weighting_with_beam_speedup_Jun2023"]
+versions_list = [
+    "rlb_image_LWA_data_test_mwa_beam_import_Jun2023",
+    "rlb_image_LWA_data_test_jones_transpose_Jun2023"
+]
 uvfits_path = "/safepool/rbyrne/lwa_data"
 outdir = "/safepool/rbyrne/fhd_outputs"
 run_fhd = True
