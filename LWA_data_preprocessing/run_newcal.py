fde81c73a44d0ffabbcd37b3f4d7c686e5ce817f
Ruby Byrne <rlbyrne@uw.edu>
2023-07-18 22:37:19 -0700

    new newcal run

--- LWA_data_preprocessing/run_newcal.py
+++ LWA_data_preprocessing/run_newcal.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pyuvdata
-from newcal import calibration
+from newcal import calibration_wrappers, calibration_optimization
 
 
 data = pyuvdata.UVData()
@@ -14,31 +14,38 @@ model.read("/home/rbyrne/calibration_testing_Jul2023/20230309_225134_73MHz_model
     Nbls,
     Ntimes,
     Nfreqs,
+    N_feed_pols,
     model_visibilities,
     data_visibilities,
     visibility_weights,
     gains_exp_mat_1,
     gains_exp_mat_2,
-) = calibration.uvdata_calibration_setup(
+    antenna_names,
+) = calibration_wrappers.uvdata_calibration_setup(
     data,
     model,
     gain_init_calfile=None,
     gain_init_stddev=0.0,
     N_feed_pols=2,
+    min_cal_baseline=30,
 )
 
-gains_fit = calibration.run_calibration_optimization_per_pol(
+gains_fit = calibration_wrappers.calibration_per_pol(
     gains_init,
     Nants,
     Nbls,
     Nfreqs,
-    2,
+    N_feed_pols,
     model_visibilities,
     data_visibilities,
     visibility_weights,
     gains_exp_mat_1,
     gains_exp_mat_2,
-    0.01,
-    xtol=1e-8,
-    verbose=False,
+    100,
+    xtol=1e-5,
+    parallel=True,
+    verbose=True,
 )
+
+cal = calibration_optimization.create_uvcal_obj(data, antenna_names, gains=gains_fit)
+cal.write_calfits("/home/rbyrne/calibration_testing_Jul2023/20230309_225134_73MHz.calfits")
