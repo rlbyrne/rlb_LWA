bbcdcd4fc8e8774b3e13ef25de01954da9641b86
Ruby Byrne <rlbyrne@uw.edu>
2023-08-05 17:57:18 -0700

    newcal run

--- LWA_data_preprocessing/run_newcal.py
+++ LWA_data_preprocessing/run_newcal.py
@@ -4,11 +4,10 @@ from newcal import calibration_wrappers, calibration_optimization
 
 
 data = pyuvdata.UVData()
-data.read("/data10/rbyrne/24-hour-run-flagged/20230309_225134_73MHz.ms")
+data.read("/data03/rbyrne/20230801_091100-091600_73MHz.uvfits")
+data.downsample_in_time(n_times_to_avg=data.Ntimes)  # Added to match model
 model = pyuvdata.UVData()
-model.read(
-    "/home/rbyrne/calibration_testing_Jul2023/20230309_225134_73MHz_model.uvfits"
-)
+model.read("/data03/rbyrne/20230801_091100-091600_73MHz_model.uvfits")
 
 (
     gains_init,
@@ -46,15 +45,15 @@ gains_fit = calibration_wrappers.calibration_per_pol(
     xtol=1e-3,
     parallel=True,
     verbose=True,
-    log_file_path="/home/rbyrne/calibration_testing_Jul2023/20230309_225134_73MHz_cal_log.txt",
+    log_file_path="/data03/rbyrne/20230801_091100-091600_73MHz_cal_log.txt",
 )
 
 cal = calibration_optimization.create_uvcal_obj(data, antenna_names, gains=gains_fit)
 cal.write_calfits(
-    "/home/rbyrne/calibration_testing_Jul2023/20230309_225134_73MHz.calfits",
+    "/data03/rbyrne/20230801_091100-091600_73MHz.calfits",
     clobber=True,
 )
-pyuvdata.utils.uvcalibrate(data, cal, inplace=True, time_check=True)
+pyuvdata.utils.uvcalibrate(data, cal, inplace=True, time_check=False)
 data.write_uvfits(
-    "/home/rbyrne/calibration_testing_Jul2023/20230309_225134_73MHz_calibrated.uvfits"
+    "/data03/rbyrne/20230801_091100-091600_73MHz_calibrated.uvfits"
 )
