061c45253f506b5c758497ae3d95b6747f75c3f8
Ruby Byrne <rlbyrne@uw.edu>
2023-09-20 16:38:31 -0700

    run new calibration with antenna flagging

--- LWA_data_preprocessing/run_newcal.py
+++ LWA_data_preprocessing/run_newcal.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyuvdata
+import LWA_preprocessing
 from newcal import calibration_wrappers, calibration_optimization, calibration_qa
 
 
@@ -212,5 +213,104 @@ def antenna_dropout_testing_Sep15():
     )
 
 
+def apply_antenna_flagging_and_recalibrate_Sept20():
+
+    with open("/Users/ruby/Astro/test_newcal_Jul2023/20230801_091100-091600_73MHz_iter1_per_ant_cost.npy", "rb") as f:
+        per_ant_cost = np.load(f)
+    f.close()
+    with open("/Users/ruby/Astro/test_newcal_Jul2023/20230801_091100-091600_73MHz_iter1_ant_names.npy", "rb") as f:
+        antenna_names_per_ant_cost = np.load(f)
+    f.close()
+
+    data = pyuvdata.UVData()
+    data.read("/data03/rbyrne/20230801_091100-091600_73MHz.uvfits")
+    data.downsample_in_time(n_times_to_avg=data.Ntimes)
+    model = pyuvdata.UVData()
+    model.read("/data03/rbyrne/20230801_091100-091600_73MHz_model.uvfits")
+
+    (
+        gains_init,
+        Nants,
+        Nbls,
+        Ntimes,
+        Nfreqs,
+        N_feed_pols,
+        model_visibilities,
+        data_visibilities,
+        visibility_weights,
+        gains_exp_mat_1,
+        gains_exp_mat_2,
+        antenna_names,
+    ) = calibration_wrappers.uvdata_calibration_setup(
+        data,
+        model,
+        gain_init_calfile="/data03/rbyrne/20230801_091100-091600_73MHz.calfits",
+        gain_init_stddev=0.0,
+        N_feed_pols=2,
+        min_cal_baseline=30,
+    )
+
+    # Make sure antenna ordering matches
+    antenna_inds = np.array([list(antenna_names_per_ant_cost).index(name) for name in antenna_names])
+    per_ant_cost = per_ant_cost[antenna_inds, :]
+
+    flag_antenna_list, visibility_weights = calibration_qa.get_antenna_flags_from_per_ant_cost(
+        per_ant_cost,
+        antenna_names,
+        flagging_threshold=2.5,
+        visibility_weights=visibility_weights,
+        gains_exp_mat_1=gains_exp_mat_1,
+        gains_exp_mat_2=gains_exp_mat_2,
+    )
+
+    gains_fit = calibration_wrappers.calibration_per_pol(
+        gains_init,
+        Nants,
+        Nbls,
+        Nfreqs,
+        N_feed_pols,
+        model_visibilities,
+        data_visibilities,
+        visibility_weights,
+        gains_exp_mat_1,
+        gains_exp_mat_2,
+        xtol=1e-3,
+        parallel=True,
+        verbose=True,
+        log_file_path="/data03/rbyrne/20230801_091100-091600_73MHz_cal_iter2_log.txt",
+    )
+
+    cal = calibration_optimization.create_uvcal_obj(
+        data, antenna_names, gains=gains_fit
+    )
+    cal.write_calfits(
+        "/data03/rbyrne/20230801_091100-091600_73MHz_iter2.calfits",
+        clobber=True,
+    )
+
+    # Reload data to undo time averaging
+    data = pyuvdata.UVData()
+    data.read("/data03/rbyrne/20230801_091100-091600_73MHz.uvfits")
+
+    # Apply new antenna flagging
+    LWA_preprocessing.flag_antennas(
+        data,
+        antenna_names=flag_antenna_list[:, 0],
+        flag_pol="X",
+        inplace=True,
+    )
+    LWA_preprocessing.flag_antennas(
+        data,
+        antenna_names=flag_antenna_list[:, 1],
+        flag_pol="Y",
+        inplace=True,
+    )
+
+    pyuvdata.utils.uvcalibrate(data, cal, inplace=True, time_check=False)
+    data.write_uvfits(
+        "/data03/rbyrne/20230801_091100-091600_73MHz_calibrated_iter2.uvfits"
+    )
+
+
 if __name__ == "__main__":
-    antenna_dropout_testing_Sep15()
+    apply_antenna_flagging_and_recalibrate_Sept20()
