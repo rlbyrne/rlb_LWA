83bdf9831653f9a65e8b715abc14a4d3220cfead
Ruby Byrne <rlbyrne@uw.edu>
2023-11-28 20:37:37 -0800

    variable name fix

--- LWA_data_preprocessing/run_generate_model_vis.sh
+++ LWA_data_preprocessing/run_generate_model_vis.sh
@@ -5,7 +5,7 @@ beam_file=/home/rbyrne/rlb_LWA/LWAbeam_2015.fits
 output_path=/data03/rbyrne/20230819/simulation_outputs
 catalog_path=/fast/rbyrne/skymodels
 #mpirun -n 20 python ${script_path} ${catalog_path}/cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_cyg_cas_sim.uvfits
-mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_cyg_cas_sim.uvfits
-mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_sources.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_sources_sim.uvfits
-mpirun -n 20 python ${script_path} ${catalog_path}/FullVLSSCatalog.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_VLSS_sim.uvfits
+#mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_cyg_cas_sim.uvfits
+#mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_sources.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_sources_sim.uvfits
+#mpirun -n 20 python ${script_path} ${catalog_path}/FullVLSSCatalog.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_VLSS_sim.uvfits
 mpirun -n 20 python /home/rbyrne/rlb_LWA/LWA_data_preprocessing/generate_model_vis_diffuse.py ${catalog_path}/ovro_lwa_sky_map_73.152MHz.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_mmode_sim.uvfits
