f3978178676420b8962456fa9a8ffd2b869b5dc0
Ruby Byrne <rbyrne@lwacalim03.mcs.pvt>
2023-12-12 11:03:36 +0000

    rename new simulation

--- LWA_data_preprocessing/run_generate_model_vis.sh
+++ LWA_data_preprocessing/run_generate_model_vis.sh
@@ -1,7 +1,7 @@
 script_path=/home/rbyrne/rlb_LWA/LWA_data_preprocessing/generate_model_vis.py
 obsname=20230819_093023_73MHz
 input_obs=/data03/rbyrne/20230819/${obsname}.ms
-beam_file=/home/rbyrne/rlb_LWA/LWAbeam_2015.fits
+#beam_file=/home/rbyrne/rlb_LWA/LWAbeam_2015.fits
 output_path=/data03/rbyrne/20230819/simulation_outputs
 catalog_path=/fast/rbyrne/skymodels
 #mpirun -n 20 python ${script_path} ${catalog_path}/cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_cyg_cas_sim.uvfits
@@ -13,4 +13,4 @@ catalog_path=/fast/rbyrne/skymodels
 
 # Test Nivedita's beam
 beam_file=/data03/rbyrne/LWA_10to100.beamfits
-mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_cyg_cas_sim.uvfits
+mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_cyg_cas_sim_NMbeam.uvfits
