cf7aae4d23d6d5bbc41bda52b1287b93be0a67c0
Ruby Byrne <rlbyrne@uw.edu>
2023-12-14 16:06:51 +0100

    plot total power

--- plot_kpar0_fractional_power.py
+++ plot_kpar0_fractional_power.py
@@ -23,5 +23,33 @@ for run_ind, run_path in enumerate(run_filepaths):
     )
     plt.plot(k_edges_plot, power_plot, color=colors[run_ind], label=run_names[run_ind])
     plt.xscale("log")
+    plt.xlabel("k-perpendicular (h/Mpc)")
+    plt.ylabel("Fractional Power Recovered (%)")
 plt.legend()
 plt.savefig(f"{plot_save_dir}/frac_power_recovered.png")
+
+for run_ind, run_path in enumerate(run_filepaths):
+
+    residual_filename = f"{run_path}/ps/data/1d_binning/20230819_093023_73MHz__gridded_uvf_noimgclip_res_xx_dft_averemove_swbh_dencorr_k0power.idlsave"
+    residual = scipy.io.readsav(residual_filename)["power"]
+    k_edges = scipy.io.readsav(residual_filename)["k_edges"]
+    power_plot = np.repeat(residual, 2)
+    k_edges_plot = np.concatenate(
+        ([k_edges[0]], np.repeat(k_edges[1:-1], 2), [k_edges[-1]])
+    )
+    plt.plot(k_edges_plot, power_plot, color=colors[run_ind], label=f"{run_names[run_ind]} residual")
+
+    dirty_filename = f"{run_path}/ps/data/1d_binning/20230819_093023_73MHz__gridded_uvf_noimgclip_dirty_xx_dft_averemove_swbh_dencorr_k0power.idlsave"
+    dirty = scipy.io.readsav(dirty_filename)["power"]
+    k_edges = scipy.io.readsav(dirty_filename)["k_edges"]
+    power_plot = np.repeat(dirty, 2)
+    k_edges_plot = np.concatenate(
+        ([k_edges[0]], np.repeat(k_edges[1:-1], 2), [k_edges[-1]])
+    )
+    plt.plot(k_edges_plot, power_plot, color=colors[run_ind], label=f"{run_names[run_ind]} dirty")
+
+    plt.xscale("log")
+    plt.xlabel("k-perpendicular (h/Mpc)")
+    plt.ylabel("Power")
+plt.legend()
+plt.savefig(f"{plot_save_dir}/kpar0_power.png")
