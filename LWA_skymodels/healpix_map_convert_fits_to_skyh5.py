f7bf79f78875eab22994b6996e87b9fd8b731766
Ruby Byrne <rlbyrne@uw.edu>
2023-12-21 14:37:33 -0800

    restructure healpix conversion code

--- LWA_skymodels/healpix_map_convert_fits_to_skyh5.py
+++ LWA_skymodels/healpix_map_convert_fits_to_skyh5.py
@@ -4,9 +4,15 @@ import healpy as hp
 import pyradiosky
 import astropy.units as units
 from astropy.units import Quantity
+import sys
 
 
-def convert_fits_to_pyradiosky(fits_filepath, freq_mhz):
+def convert_fits_to_pyradiosky(
+    fits_filepath,
+    freq_mhz,
+    output_frame=None,  # Options are "galactic", "equatorial", or None. If None, input frame is preserved.
+    output_nside=None,  # If None, input nside is preserved
+):
 
     file_contents = fits.open(fits_filepath)
     map_data = np.array(file_contents[1].data, dtype=float)
@@ -18,10 +24,18 @@ def convert_fits_to_pyradiosky(fits_filepath, freq_mhz):
 
     history_str = f"From file {fits_filepath.split('/')[-1]}"
 
-    # Map must be in equatorial coordinates
     # COORDSYS definitions: G = galactic, E = ecliptic, C = celestial = equatorial
-    if coordsys != "C":
-        if coordsys == "G":  # Remap to equatorial coordinates
+    if output_frame is not None:
+        if output_frame == "equatorial":
+            output_frame_code = "C"
+        elif output_frame == "galactic":
+            output_frame_code = "G"
+        else:
+            sys.exit(
+                "ERROR: Unsupported output_frame. Options are equatorial, galactic, or None."
+            )
+
+        if coordsys != output_frame_code:  # Transform coordinate frame
             if ordering == "nest":
                 nest = True
             elif ordering == "ring":
@@ -29,23 +43,28 @@ def convert_fits_to_pyradiosky(fits_filepath, freq_mhz):
             else:
                 print("WARNING: Unknown ordering. Assuming ring ordering.")
                 nest = False
-            # Get pixel coordinates
-            theta_gal, phi_gal = hp.pixelfunc.pix2ang(nside, np.arange(npix), nest=nest)
-            # Convert pixel coordinates
-            rot = hp.rotator.Rotator(coord=["C", "G"])
-            theta_eq, phi_eq = rot(theta_gal, phi_gal)
-            # Interpolate map to new pixel locations
-            map_data = hp.get_interp_val(map_data, theta_eq, phi_eq, nest=nest)
-            history_str = f"{history_str}, interpolated to equatorial frame"
-        else:
-            print("WARNING: Unknown coordsys. No reordering applied.")
+            theta_init, phi_init = hp.pixelfunc.pix2ang(
+                nside, np.arange(npix), nest=nest
+            )
+            rot = hp.rotator.Rotator(coord=[coordsys, output_frame_code])
+            theta_rot, phi_rot = rot(theta_init, phi_init)
+            map_data = hp.get_interp_val(map_data, theta_rot, phi_rot, nest=nest)
+            history_str = f"{history_str}, interpolated to {output_frame} frame"
+            coordsys = output_frame_code
+
+    if coordsys == "C":
+        frame_str = "icrs"
+    elif coordsys == "G":
+        frame_str = "galactic"
+    else:
+        sys.exit("ERROR: Unsupported coordsys.")
 
     skymodel = pyradiosky.SkyModel()
     skymodel.component_type = "healpix"
     skymodel.nside = nside
     skymodel.hpx_order = ordering
-    skymodel.frame = "icrs"
-    skymodel.hpx_frame = "icrs"
+    skymodel.frame = frame_str
+    skymodel.hpx_frame = frame_str
     skymodel.Nfreqs = 1
     skymodel.Ncomponents = npix
     skymodel.freq_array = Quantity(np.full(skymodel.Nfreqs, freq_mhz * 1e6), "hertz")
@@ -64,9 +83,7 @@ if __name__ == "__main__":
 
     # Map is downloaded from https://lambda.gsfc.nasa.gov/product/foreground/fg_ovrolwa_radio_maps_get.html
 
-    fits_filepath = (
-        "/safepool/rbyrne/skymodels/ovro_lwa_sky_map_73.152MHz.fits"
-    )
+    fits_filepath = "/safepool/rbyrne/skymodels/ovro_lwa_sky_map_73.152MHz.fits"
     skymodel = convert_fits_to_pyradiosky(fits_filepath, 73.152)
     skymodel.check()
     skymodel.write_skyh5(
