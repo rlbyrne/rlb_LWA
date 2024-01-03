b67078ab1a3632bb851bc82e287fb215c2592c04
Ruby Byrne <rbyrne@caltech.edu>
2024-01-02 18:19:46 -0800

    log yscale

--- plot_kpar0_fractional_power.py
+++ plot_kpar0_fractional_power.py
@@ -76,7 +76,7 @@ for run_ind, run_path in enumerate(run_filepaths):
     )
 
     plt.xscale("log")
-    plt.ylim([0,1e4])
+    plt.yscale("log")
     plt.xlabel("k-perpendicular (h/Mpc)")
     plt.ylabel("Power")
 plt.legend()
