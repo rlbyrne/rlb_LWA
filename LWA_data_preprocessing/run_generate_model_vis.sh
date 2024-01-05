cfa180782f2b6eff3504452f1f19f11926be6021
Ruby Byrne <rlbyrne@uw.edu>
2024-01-04 17:16:13 -0800

    correct call

--- LWA_data_preprocessing/run_generate_model_vis.sh
+++ LWA_data_preprocessing/run_generate_model_vis.sh
@@ -1,7 +1,7 @@
 script_path=/home/rbyrne/rlb_LWA/LWA_data_preprocessing/generate_model_vis.py
 obsname=20230819_093023_73MHz
 input_obs=/data03/rbyrne/20230819/${obsname}.ms
-#beam_file=/home/rbyrne/rlb_LWA/LWAbeam_2015.fits
+beam_file=/home/rbyrne/rlb_LWA/LWAbeam_2015.fits
 output_path=/data03/rbyrne/20230819/simulation_outputs
 catalog_path=/fast/rbyrne/skymodels
 #mpirun -n 20 python ${script_path} ${catalog_path}/cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_cyg_cas_sim.uvfits
