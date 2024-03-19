a4b9c163d5d372fd03f6235c6dc17e0b50b0b349
Ruby Byrne <rlbyrne@uw.edu>
2024-03-19 13:57:18 -0700

    test lambda=0

--- LWA_data_preprocessing/run_newcal.py
+++ LWA_data_preprocessing/run_newcal.py
@@ -494,5 +494,37 @@ def test_single_time_recalibrate_perturbed_Mar19():
     )
 
 
+def test_single_time_lambda0_Mar19():
+
+    data = pyuvdata.UVData()
+    data.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_data.ms")
+    model = pyuvdata.UVData()
+    model.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_model.ms")
+
+    caldata_obj = calibration_wrappers.CalData()
+    caldata_obj.load_data(data, model, min_cal_baseline_lambda=15, lambda_val=0.)
+    calibration_wrappers.calibration_per_pol(
+        caldata_obj,
+        verbose=True,
+        log_file_path="/data03/rbyrne/20231222/newcal_single_time/calibration_logs/newcal_lambda0_Mar19.txt",
+    )
+    uvcal = caldata_obj.convert_to_uvcal()
+    uvcal.write_calfits(
+        "/data03/rbyrne/20231222/newcal_single_time/cal46_small_lambda0.calfits",
+        clobber=True,
+    )
+
+    # Apply calibration
+    data = pyuvdata.UVData()
+    data.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_data.ms")
+    pyuvdata.utils.uvcalibrate(data, uvcal, inplace=True, time_check=False)
+    data.reorder_pols(order="CASA", run_check=False)
+    data.write_ms(
+        "/data03/rbyrne/20231222/newcal_single_time/cal46_small_newcal_calibrated_lambda0.ms",
+        flip_conj=True,
+        run_check=False,
+    )
+
+
 if __name__ == "__main__":
-    test_single_time_recalibrate_perturbed_Mar19()
+    test_single_time_lambda0_Mar19()
