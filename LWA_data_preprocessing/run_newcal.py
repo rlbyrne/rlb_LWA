469fb42238e877377b2b8ae64e507b24815266eb
Ruby Byrne <rlbyrne@uw.edu>
2024-03-20 15:03:34 -0700

    debugging test

--- LWA_data_preprocessing/run_newcal.py
+++ LWA_data_preprocessing/run_newcal.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pyuvdata
 import LWA_preprocessing
-from newcal import calibration_wrappers, calibration_optimization, cost_function_calculations, calibration_qa
+from newcal import (
+    calibration_wrappers,
+    calibration_optimization,
+    cost_function_calculations,
+    calibration_qa,
+)
 
 
 def calibrate_Aug4():
@@ -144,13 +149,13 @@ def debug_Aug21():
 def antenna_dropout_testing_Sep15():
 
     # Plot gains:
-    #cal = pyuvdata.UVCal()
-    #cal.read_calfits("/data03/rbyrne/20230801_091100-091600_73MHz.calfits")
-    #calibration_optimization.plot_gains(
+    # cal = pyuvdata.UVCal()
+    # cal.read_calfits("/data03/rbyrne/20230801_091100-091600_73MHz.calfits")
+    # calibration_optimization.plot_gains(
     #    cal,
     #    "/data03/rbyrne/antenna_dropout_testing",
     #    plot_prefix="20230801_091100-091600_73MHz_iter1",
-    #)
+    # )
 
     data = pyuvdata.UVData()
     data.read("/data03/rbyrne/20230801_091100-091600_73MHz_calibrated.uvfits")
@@ -215,10 +220,16 @@ def antenna_dropout_testing_Sep15():
 
 def apply_antenna_flagging_and_recalibrate_Sept20():
 
-    with open("/data03/rbyrne/antenna_dropout_testing/20230801_091100-091600_73MHz_iter1_per_ant_cost.npy", "rb") as f:
+    with open(
+        "/data03/rbyrne/antenna_dropout_testing/20230801_091100-091600_73MHz_iter1_per_ant_cost.npy",
+        "rb",
+    ) as f:
         per_ant_cost = np.load(f)
     f.close()
-    with open("/data03/rbyrne/antenna_dropout_testing/20230801_091100-091600_73MHz_iter1_ant_names.npy", "rb") as f:
+    with open(
+        "/data03/rbyrne/antenna_dropout_testing/20230801_091100-091600_73MHz_iter1_ant_names.npy",
+        "rb",
+    ) as f:
         antenna_names_per_ant_cost = np.load(f)
     f.close()
 
@@ -250,10 +261,15 @@ def apply_antenna_flagging_and_recalibrate_Sept20():
     )
 
     # Make sure antenna ordering matches
-    antenna_inds = np.array([list(antenna_names_per_ant_cost).index(name) for name in antenna_names])
+    antenna_inds = np.array(
+        [list(antenna_names_per_ant_cost).index(name) for name in antenna_names]
+    )
     per_ant_cost = per_ant_cost[antenna_inds, :]
 
-    flag_antenna_list, visibility_weights = calibration_qa.get_antenna_flags_from_per_ant_cost(
+    (
+        flag_antenna_list,
+        visibility_weights,
+    ) = calibration_qa.get_antenna_flags_from_per_ant_cost(
         per_ant_cost,
         antenna_names,
         flagging_threshold=2.5,
@@ -330,6 +346,7 @@ def casa_calibration_comparison_Feb23():
         "/data03/rbyrne/20231222/newcal_calibration/cal46.calfits",
         clobber=True,
     )
+
 
 def apply_calibration_Mar2():
 
@@ -433,7 +450,9 @@ def test_single_time_Mar19():
 def test_single_time_recalibrate_Mar19():
 
     data = pyuvdata.UVData()
-    data.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_casa_calibrated.ms")
+    data.read_ms(
+        "/data03/rbyrne/20231222/newcal_single_time/cal46_small_casa_calibrated.ms"
+    )
     model = pyuvdata.UVData()
     model.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_model.ms")
 
@@ -452,7 +471,9 @@ def test_single_time_recalibrate_Mar19():
 
     # Apply calibration
     data = pyuvdata.UVData()
-    data.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_casa_calibrated.ms")
+    data.read_ms(
+        "/data03/rbyrne/20231222/newcal_single_time/cal46_small_casa_calibrated.ms"
+    )
     pyuvdata.utils.uvcalibrate(data, uvcal, inplace=True, time_check=False)
     data.reorder_pols(order="CASA", run_check=False)
     data.write_ms(
@@ -465,7 +486,9 @@ def test_single_time_recalibrate_Mar19():
 def test_single_time_recalibrate_perturbed_Mar19():
 
     data = pyuvdata.UVData()
-    data.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_casa_calibrated.ms")
+    data.read_ms(
+        "/data03/rbyrne/20231222/newcal_single_time/cal46_small_casa_calibrated.ms"
+    )
     model = pyuvdata.UVData()
     model.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_model.ms")
 
@@ -484,7 +507,9 @@ def test_single_time_recalibrate_perturbed_Mar19():
 
     # Apply calibration
     data = pyuvdata.UVData()
-    data.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_casa_calibrated.ms")
+    data.read_ms(
+        "/data03/rbyrne/20231222/newcal_single_time/cal46_small_casa_calibrated.ms"
+    )
     pyuvdata.utils.uvcalibrate(data, uvcal, inplace=True, time_check=False)
     data.reorder_pols(order="CASA", run_check=False)
     data.write_ms(
@@ -502,7 +527,7 @@ def test_single_time_lambda0_Mar19():
     model.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_model.ms")
 
     caldata_obj = calibration_wrappers.CalData()
-    caldata_obj.load_data(data, model, min_cal_baseline_lambda=15, lambda_val=0.)
+    caldata_obj.load_data(data, model, min_cal_baseline_lambda=15, lambda_val=0.0)
     calibration_wrappers.calibration_per_pol(
         caldata_obj,
         verbose=True,
@@ -526,5 +551,61 @@ def test_single_time_lambda0_Mar19():
     )
 
 
+def compare_cost_values_Mar20():
+
+    model = pyuvdata.UVData()
+    model.read_ms("/data03/rbyrne/20231222/newcal_single_time/cal46_small_model.ms")
+    model.select(frequency=47851562.5)
+    data_recalibrated = pyuvdata.UVData()
+    data_recalibrated.read_ms(
+        "/data03/rbyrne/20231222/newcal_single_time/cal46_small_newcal_recalibrated.ms"
+    )
+    data_recalibrated.select(frequency=47851562.5)
+    data_calibrated = pyuvdata.UVData()
+    data_calibrated.read_ms(
+        "/data03/rbyrne/20231222/newcal_single_time/cal46_small_newcal_calibrated.ms"
+    )
+    data_calibrated.select(frequency=47851562.5)
+    use_pol = 0
+
+    caldata_recalibrated = calibration_wrappers.CalData()
+    caldata_recalibrated.load_data(
+        data_recalibrated,
+        model,
+        gain_init_to_vis_ratio=False,
+        min_cal_baseline_lambda=15,
+        lambda_val=0.0,
+    )
+    recalibrated_cost = cost_function_calculations.cost_function_single_pol(
+        caldata_recalibrated.gains[:, 0, use_pol],
+        caldata_recalibrated.model_visibilities[0, :, 0, use_pol],
+        caldata_recalibrated.data_visibilities[0, :, 0, use_pol],
+        caldata_recalibrated.visibility_weights[0, :, 0, use_pol],
+        caldata_recalibrated.gains_exp_mat_1,
+        caldata_recalibrated.gains_exp_mat_2,
+        caldata_recalibrated.lambda_val,
+    )
+    print(f"Recalibration run cost: {recalibrated_cost}")
+
+    caldata_calibrated = calibration_wrappers.CalData()
+    caldata_calibrated.load_data(
+        data_calibrated,
+        model,
+        gain_init_to_vis_ratio=False,
+        min_cal_baseline_lambda=15,
+        lambda_val=0.0,
+    )
+    calibrated_cost = cost_function_calculations.cost_function_single_pol(
+        caldata_calibrated.gains[:, 0, use_pol],
+        caldata_calibrated.model_visibilities[0, :, 0, use_pol],
+        caldata_calibrated.data_visibilities[0, :, 0, use_pol],
+        caldata_calibrated.visibility_weights[0, :, 0, use_pol],
+        caldata_calibrated.gains_exp_mat_1,
+        caldata_calibrated.gains_exp_mat_2,
+        caldata_calibrated.lambda_val,
+    )
+    print(f"Calibration run cost: {calibrated_cost}")
+
+
 if __name__ == "__main__":
-    test_single_time_lambda0_Mar19()
+    compare_cost_values_Mar20()
