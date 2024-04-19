1cbcf2e4a7cc0732cd70ff9369f1020ad2fdddbc
Ruby Byrne <rlbyrne@uw.edu>
2024-04-19 16:46:00 -0700

    rerun diffuse simulation

--- LWA_data_preprocessing/run_generate_model_vis.sh
+++ LWA_data_preprocessing/run_generate_model_vis.sh
@@ -5,14 +5,14 @@ beam_file=/home/rbyrne/rlb_LWA/LWAbeam_2015.fits
 output_path=/data03/rbyrne/20231222/test_pyuvsim_modeling
 catalog_path=/fast/rbyrne/skymodels
 #mpirun -n 20 python ${script_path} ${catalog_path}/cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_cyg_cas_sim.uvfits
-mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_cyg_cas_sim.uvfits
-mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_sources.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_sources_sim.uvfits
-mpirun -n 20 python ${script_path} ${catalog_path}/FullVLSSCatalog.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_VLSS_sim.uvfits
+#mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_cyg_cas.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_cyg_cas_sim.uvfits
+#mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_sources.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_sources_sim.uvfits
+#mpirun -n 20 python ${script_path} ${catalog_path}/FullVLSSCatalog.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_VLSS_sim.uvfits
 
 # Run diffuse
 diffuse_script_path=/home/rbyrne/rlb_LWA/LWA_data_preprocessing/generate_model_vis_diffuse.py
-mpirun -n 15 python ${diffuse_script_path} ${catalog_path}/ovro_lwa_sky_map_73.152MHz_equatorial_nside128.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_mmode_sim.uvfits
+mpirun -n 15 python ${diffuse_script_path} ${catalog_path}/ovro_lwa_sky_map_73.152MHz_nside128.skyh5 ${beam_file} ${input_obs} ${output_path}/${obsname}_mmode_sim.uvfits
 
 # Test Nivedita's beam
-nm_beam_file=/data03/rbyrne/LWA_10to100.beamfits
-mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_cyg_cas.skyh5 ${nm_beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_cyg_cas_sim_NMbeam.uvfits
+#nm_beam_file=/data03/rbyrne/LWA_10to100.beamfits
+#mpirun -n 20 python ${script_path} ${catalog_path}/Gasperin2020_cyg_cas.skyh5 ${nm_beam_file} ${input_obs} ${output_path}/${obsname}_deGasperin_cyg_cas_sim_NMbeam.uvfits
