9aee5138b5f9462b85b9bc4862c922a0a2c19899
Ruby Byrne <rlbyrne@uw.edu>
2024-04-19 16:41:56 -0700

    rerun downsample with proper coordinates

--- LWA_skymodels/downsample_healpix.py
+++ LWA_skymodels/downsample_healpix.py
@@ -7,14 +7,17 @@ import pyuvdata
 import pyuvsim
 
 
-diffuse_map_path = "/safepool/rbyrne/skymodels/ovro_lwa_sky_map_73.152MHz_equatorial.skyh5"
+diffuse_map_path = "/fast/rbyrne/skymodels/ovro_lwa_sky_map_73.152MHz.skyh5"
 diffuse_map = pyradiosky.SkyModel()
 diffuse_map.read_skyh5(diffuse_map_path)
 print(diffuse_map.nside)
 
 use_nside = 128
 downsampled_map_data = hp.pixelfunc.ud_grade(
-    diffuse_map.stokes[0, 0, :].value, use_nside, pess=True, order_in=diffuse_map.hpx_order
+    diffuse_map.stokes[0, 0, :].value,
+    use_nside,
+    pess=True,
+    order_in=diffuse_map.hpx_order,
 )
 diffuse_map.nside = use_nside
 diffuse_map.Ncomponents = hp.nside2npix(use_nside)
@@ -25,7 +28,7 @@ diffuse_map.stokes[0, 0, :] = downsampled_map_data * units.Kelvin
 diffuse_map.hpx_inds = np.arange(diffuse_map.Ncomponents)
 diffuse_map.check()
 diffuse_map.write_skyh5(
-    "/safepool/rbyrne/skymodels/ovro_lwa_sky_map_73.152MHz_equatorial_nside128.skyh5",
+    "/safepool/rbyrne/skymodels/ovro_lwa_sky_map_73.152MHz_nside128.skyh5",
     run_check=True,
     clobber=True,
 )
