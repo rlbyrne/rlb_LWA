d3618c18d7ec34ab17dbf5cd5388b41486bbb331
Ruby Byrne <rlbyrne@uw.edu>
2024-07-17 15:35:00 -0700

    more simulation runs

--- LWA_data_preprocessing/run_generate_model_vis.sh
+++ LWA_data_preprocessing/run_generate_model_vis.sh
@@ -26,7 +26,7 @@ python /home/rbyrne/rlb_LWA/LWA_data_preprocessing/generate_model_vis_matvis.py 
 map_path="/fast/rbyrne/skymodels/ovro_lwa_sky_map_46.992MHz_nside512.skyh5"
 beam_path="/data03/rbyrne/LWA_10to100.beamfits"
 input_obs="/data03/rbyrne/20231222/test_pyuvsim_modeling/cal46_time11_conj.ms"
-output_uvfits_path="/data03/rbyrne/20231222/compare_lsts/cal46_time11_mmode_matvis_sim_nside512.uvfits"
+output_uvfits_path="/data03/rbyrne/20231222/compare_lsts/cal46_time11_mmode_matvis_sim_nside512_testgpu.uvfits"
 python /home/rbyrne/rlb_LWA/LWA_data_preprocessing/generate_model_vis_matvis.py ${map_path} ${beam_path} ${input_obs} ${output_uvfits_path}
 
 # Run source model
