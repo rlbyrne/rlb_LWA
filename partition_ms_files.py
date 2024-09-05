76495397b4685380f317ff163dc6e613e8050bbe
Ruby Byrne <rbyrne@lwacalim03.calim.mcs.pvt>
2024-09-05 22:05:03 +0000

    update ms file processing

--- partition_ms_files.py
+++ partition_ms_files.py
@@ -8,11 +8,15 @@ use_files = ["41", "46", "50", "55", "59", "64", "73", "78", "82"]
 
 time_step = 0
 while time_step < 72:
-    if f"41-82_{dirname}_{str(time_step).zfill(3)}.ms" not in os.listdir(pathname):
+    if f"41-82_{dirname}_{str(time_step).zfill(3)}.ms" in os.listdir(pathname):
+        print(f"41-82_{dirname}_{str(time_step).zfill(3)}.ms exists. Skipping.")
+        time_step += 1
+    else:
         for filename in use_files:
             if f"{filename}_{dirname}_{str(time_step).zfill(3)}.ms" not in os.listdir(
                 pathname
             ):  # Check if file already exists
+                print(f"Partitioning file {filename}.ms")
                 partition(
                     f"{pathname}/{filename}.ms",
                     outputvis=f"{pathname}/{filename}_{dirname}_{str(time_step).zfill(3)}.ms",
@@ -23,6 +27,7 @@ while time_step < 72:
             f"{pathname}/{filename}_{dirname}_{str(time_step).zfill(3)}.ms"
             for filename in use_files
         ]
+        print(f"Concatenating files and saving to 41-82_{dirname}_{str(time_step).zfill(3)}.ms")
         virtualconcat(
             vis=datafile_list,
             concatvis=f"{pathname}/41-82_{dirname}_{str(time_step).zfill(3)}.ms",
