7215068227debfa0d9310d87de6292c2c5dd85d5
Ruby Byrne <rbyrne@lwacalim03.calim.mcs.pvt>
2025-03-19 20:13:15 +0000

    updated wrapper for josh's calibrator

--- LWA_data_preprocessing/run_joshs_calibration.py
+++ LWA_data_preprocessing/run_joshs_calibration.py
@@ -7,6 +7,8 @@ from dsa2000_cal.iterative_calibrator import (
 )
 from dsa2000_cal.probabilistic_models.gain_prior_models import GainPriorModel
 import numpy as np
+from astropy import time as at, units as au, coordinates as ac
+import time
 
 
 def data_generator():
@@ -35,8 +37,8 @@ def data_generator():
     flags = uv.data_array[np.newaxis, :, :, :]  # Shape Ntimes, Nbls, Nfreqs, Npols
     antenna1 = uv.ant_1_array
     antenna2 = uv.ant_2_array
-    times = uv.time_array[0]
-    freqs = uv.freq_array
+    times = at.Time([uv.time_array[0]], format="mjd", scale="utc")
+    freqs = au.Quantity(uv.freq_array, "Hz")
 
     # Get background
     model = pyuvdata.UVData()
@@ -86,12 +88,21 @@ def data_generator():
         antenna2=antenna2,
         model_times=times,
         model_freqs=freqs,
-        ref_time=times,
+        ref_time=times[0],
     )
 
 
 if __name__ == "__main__":
     your_generator = data_generator()
+
+    # Get antenna locations
+    # TODO: Ensure that antenna ordering matches that of data_generator
+    data_ms = "/lustre/rbyrne/2024-03-03/ddcal/20240303_133205_73MHz.ms"
+    uv = pyuvdata.UVData()
+    uv.read(data_ms)
+    telescope_ecef_xyz = au.Quantity(uv.telescope.location.geocentric).to_value("m")
+    antpos = uv.telescope.antenna_positions + telescope_ecef_xyz
+    antenna_locs = ac.EarthLocation.from_geocentric(antpos[:, 0], antpos[:, 1], antpos[:,2], unit="m")
 
     gain_prior_model = GainPriorModel(
         gain_stddev=1.0,
@@ -107,8 +118,12 @@ if __name__ == "__main__":
         run_name="demo",
         gain_probabilistic_model=gain_prior_model,
         full_stokes=True,
-        antennas=352,
+        antennas=antenna_locs,
         verbose=True,  # if using GPU's set to False
         devices=None,
     )
+    print("Starting calibration.")
+    start_time = time.time()
     calibrator.run(your_generator, Ts=None, Cs=None)
+    print("Done.")
+    print(f"Calibration timing: {(time.time() - start_time)/60.} minutes")
