387223f34556f412503d8eb126da318ba2fa6b4b
Ruby Byrne <rbyrne@lwacalim03.calim.mcs.pvt>
2025-05-13 21:29:58 +0000

    improve ms concatenation code

--- LWA_data_preprocessing/concatenate_ms_files.py
+++ LWA_data_preprocessing/concatenate_ms_files.py
@@ -1,6 +1,7 @@
 import casatasks
 import os
 import sys
+import datetime
 
 use_freq_bands = [
     "41",
@@ -14,52 +15,63 @@ use_freq_bands = [
     "78",
     "82",
 ]
+delta_time = datetime.timedelta(minutes=2)
+start_time = datetime.datetime(2025, 5, 5, 12, 56, 9)
+time_steps = 2
 
-for freq_band in use_freq_bands:
+for time_step in range(time_steps):
 
-    year = "2025"
-    month = "05"
-    day = "05"
-    min_time_str = "123005"
-    max_time_str = "123205"
+    use_start_time = start_time + time_step * delta_time
+    end_time = start_time + (time_step + 1) * delta_time
+    min_time_str = use_start_time.strftime("%H%M%S")
+    max_time_str = end_time.strftime("%H%M%S")
+    year = use_start_time.strftime("%Y")
+    month = use_start_time.strftime("%m")
+    day = use_start_time.strftime("%d")
 
-    datadir = f"/lustre/pipeline/slow/{freq_band}MHz/{year}-{month}-{day}/12"
-    copied_data_dir = f"/lustre/rbyrne/{year}-{month}-{day}"
+    for freq_band in use_freq_bands:
 
-    if not os.path.isdir(copied_data_dir):  # Make target directory if it does not exist
-        os.mkdir(copied_data_dir)
+        datadir = f"/lustre/pipeline/calibration/{freq_band}MHz/{year}-{month}-{day}/12"
+        #datadir = f"/lustre/pipeline/slow/{freq_band}MHz/{year}-{month}-{day}/12"
+        copied_data_dir = f"/lustre/rbyrne/{year}-{month}-{day}"
 
-    all_files = os.listdir(datadir)
-    use_files = [
-        filename
-        for filename in all_files
-        if filename.startswith(f"{year}{month}{day}") and filename.endswith(".ms")
-    ]
-    use_files = [
-        filename            
-        for filename in use_files
-        if (int(filename.split("_")[1]) >= int(min_time_str))
-        and int(filename.split("_")[1]) < int(max_time_str)
-    ]
-    if len(use_files) != 12:
-        print("WARNING: Number of files found is not 12.")
-        sys.exit()
+        if not os.path.isdir(copied_data_dir):  # Make target directory if it does not exist
+            os.mkdir(copied_data_dir)
 
-    use_files.sort()
-    output_filename = f"{year}{month}{day}_{use_files[0].split('_')[1]}-{use_files[-1].split('_')[1]}_{freq_band}MHz.ms"
+        all_files = os.listdir(datadir)
+        use_files = [
+            filename
+            for filename in all_files
+            if filename.startswith(f"{year}{month}{day}") and filename.endswith(".ms")
+        ]
+        print(use_files)
+        print(min_time_str)
+        print(max_time_str)
+        use_files = [
+            filename            
+            for filename in use_files
+            if (int(filename.split("_")[1]) >= int(min_time_str))
+            and int(filename.split("_")[1]) < int(max_time_str)
+        ]
+        if len(use_files) != 12:
+            print("ERROR: Number of files found is not 12.")
+            sys.exit()
 
-    if not os.path.isdir(f"{copied_data_dir}/{output_filename}"):
-        # Copy files
-        for filename in use_files:
-            if not os.path.isfile(f"{copied_data_dir}/{filename}"):
-                print(f"Copying file {filename}")
-                os.system(f"cp -r {datadir}/{filename} {copied_data_dir}/{filename}")
-        use_files_full_paths = [f"{copied_data_dir}/{filename}" for filename in use_files]
+        use_files.sort()
+        output_filename = f"{year}{month}{day}_{use_files[0].split('_')[1]}-{use_files[-1].split('_')[1]}_{freq_band}MHz.ms"
 
-        # Concatenate files
-        casatasks.virtualconcat(
-            vis=use_files_full_paths, concatvis=f"{copied_data_dir}/{output_filename}"
-        )
+        if not os.path.isdir(f"{copied_data_dir}/{output_filename}"):
+            # Copy files
+            for filename in use_files:
+                if not os.path.isfile(f"{copied_data_dir}/{filename}"):
+                    print(f"Copying file {filename}")
+                    os.system(f"cp -r {datadir}/{filename} {copied_data_dir}/{filename}")
+            use_files_full_paths = [f"{copied_data_dir}/{filename}" for filename in use_files]
 
-        # Run aoflagger
-        os.system(f"aoflagger {copied_data_dir}/{output_filename}")
+            # Concatenate files
+            casatasks.virtualconcat(
+                vis=use_files_full_paths, concatvis=f"{copied_data_dir}/{output_filename}"
+            )
+
+            # Run aoflagger
+            os.system(f"aoflagger {copied_data_dir}/{output_filename}")
