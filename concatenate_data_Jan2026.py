40bb74428db0ea9c3568b35ec4b69de88f718f22
Ruby Byrne <rbyrne@lwacalim09.calim.mcs.pvt>
2026-01-14 02:07:13 +0000

    add data deletion

--- concatenate_data_Jan2026.py
+++ concatenate_data_Jan2026.py
@@ -28,6 +28,49 @@ freq_intervals = [
 dates = ["2026-01-07"]
 
 
+def chunk_in_frequency(uv, filenames):
+
+    out_filepaths = []
+    for interval in np.array(freq_intervals):
+        keep_freqs = np.where(
+            (uv.freq_array >= interval[0]) & (uv.freq_array <= interval[1])
+        )[0]
+        uv_freq_band = uv.select(frequencies=uv.freq_array[keep_freqs], inplace=False)
+
+        mean_freq = int(np.mean(uv.freq_array[keep_freqs] / 1e6))
+        year = filenames[0][:4]
+        month = filenames[0][4:6]
+        day = filenames[0][6:8]
+        hour = filenames[0][9:11]
+        start_minute = filenames[0][11:13]
+        start_second = filenames[0][13:15]
+        end_minute = filenames[-1][11:13]
+        end_second = filenames[-1][13:15]
+
+        outdir = f"/lustre/pipeline/cosmology/concatenated_data/{mean_freq}MHz/{year}-{month}-{day}/{hour}"
+        out_filename = f"{year}{month}{day}_{hour}{start_minute}{start_second}-{hour}{end_minute}{end_second}_{mean_freq}MHz.ms"
+        if not os.path.isdir(outdir):
+            os.system(f"sudo mkdir -p {outdir}")
+        uv.write_ms(f"{outdir}/{out_filename}")
+        out_filepaths.append(f"{outdir}/{out_filename}")
+
+    return out_filepaths
+
+
+def delete_original_data(filenames):
+
+    for file_ind, filename in enumerate(filenames):
+        for freq_ind, freq in enumerate(use_freqs):
+            year = filename[:4]
+            month = filename[4:6]
+            day = filename[6:8]
+            hour = filename[9:11]
+            minute = filename[11:13]
+            second = filename[13:15]
+            path = f"/lustre/pipeline/cosmology/{freq}MHz/{year}-{month}-{day}/{hour}/{year}{month}{day}_{hour}{minute}{second}_{freq}MHz.ms"
+            os.system(f"rm -r {path}")
+
+
 def concatenate(filenames):
 
     # Check if all paths exist
@@ -67,33 +110,14 @@ def concatenate(filenames):
         else:
             uv.fast_concat(uv_freq_new, "blt", inplace=True, run_check=False)
 
-    chunk_in_frequency(uv, filenames)
-
-
-def chunk_in_frequency(uv, filenames):
-    for interval in np.array(freq_intervals):
-        keep_freqs = np.where(
-            (uv.freq_array >= interval[0]) & (uv.freq_array <= interval[1])
-        )[0]
-        uv_freq_band = uv.select(frequencies=uv.freq_array[keep_freqs], inplace=False)
-
-        mean_freq = int(np.mean(uv.freq_array[keep_freqs] / 1e6))
-        year = filenames[0][:4]
-        month = filenames[0][4:6]
-        day = filenames[0][6:8]
-        hour = filenames[0][9:11]
-        start_minute = filenames[0][11:13]
-        start_second = filenames[0][13:15]
-        end_minute = filenames[-1][11:13]
-        end_second = filenames[-1][13:15]
-
-        outdir = f"/lustre/pipeline/cosmology/concatenated_data/{mean_freq}MHz/{year}-{month}-{day}/{hour}"
-        out_filename = f"{year}{month}{day}_{hour}{start_minute}{start_second}-{hour}{end_minute}{end_second}_{mean_freq}MHz.ms"
-        if not os.path.isdir(outdir):
-            os.system(f"sudo mkdir -p {outdir}")
-        uv.write_ms(f"{outdir}/{out_filename}")
-
-        sys.exit()
+    out_filepaths = chunk_in_frequency(uv, filenames)
+    delete_data = True
+    for filepath in out_filepaths:
+        if not os.path.isdir(filepath):
+            delete_data = False
+            break
+    if delete_data:
+        delete_original_data(filenames)
 
 
 for date in dates:
