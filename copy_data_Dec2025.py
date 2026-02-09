5774be7e9cf5fd93ffa6447b3e2c79dcf47143d3
Ruby Byrne <rbyrne@lwacalim09.calim.mcs.pvt>
2026-02-09 20:46:58 +0000

    replace old data with new

--- copy_data_Dec2025.py
+++ copy_data_Dec2025.py
@@ -25,7 +25,7 @@ time_ranges = [
     ["013000", "020000"],  # half an hour of calibration data
     ["110000", "140000"],  # three hours of cosmology data
 ]
-use_dates = ["2026-02-01", "2026-02-02"]
+use_dates = ["2026-02-07", "2026-02-08"]
 
 dates = np.sort(os.listdir(f"{source_dir}/{copy_freqs[0]}MHz"))
 dates = [date for date in dates if date in use_dates]
